3b1e6b9fed1cf3e4f721a20778180c625677ffa1
Chris Wood <chris@flipbit.co.uk>
2019-06-05 08:20:52 +0100

    Add new templates, add parsing tests

---
+++ Whois.Tests/ResourceReaderTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+
+namespace Whois
+{
+    [TestFixture]
+    public class ResourceReaderTests
+    {
+        private ResourceReader reader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            reader = new ResourceReader();
+        }
+
+        [Test]
+        public void TestGetNames()
+        {
+            var names = reader.GetNames("capetown-whois.registry.net.za", "capetown");
+
+            Assert.AreEqual(2, names.Count);
+            Assert.AreEqual("Whois.Resources.capetown_whois.registry.net.za.capetown.Found.txt", names[0]);
+            Assert.AreEqual("Whois.Resources.capetown_whois.registry.net.za.capetown.NotFound.txt", names[1]);
+        }
+
+        [Test]
+        public void TestGetNamesWithDifferentCase()
+        {
+            var names = reader.GetNames("Capetown-whois.registry.net.za", "Capetown");
+
+            Assert.AreEqual(2, names.Count);
+            Assert.AreEqual("Whois.Resources.capetown_whois.registry.net.za.capetown.Found.txt", names[0]);
+            Assert.AreEqual("Whois.Resources.capetown_whois.registry.net.za.capetown.NotFound.txt", names[1]);
+        }
+
+        [Test]
+        public void TestGetNamesWhenNotFound()
+        {
+            var names = reader.GetNames("missing.server", "missing.tld");
+
+            Assert.AreEqual(0, names.Count);
+        }
+
+        [Test]
+        public void TestGetNamesWhenEmptyInputs()
+        {
+            var names = reader.GetNames(string.Empty, string.Empty);
+
+            Assert.AreEqual(0, names.Count);
+        }
+
+        [Test]
+        public void TestGetNamesWhenNullInputs()
+        {
+            var names = reader.GetNames(null, null);
+
+            Assert.AreEqual(0, names.Count);
+        }
+
+        [Test]
+        public void TestGetContent()
+        {
+            var content = reader.GetContent("Whois.Resources.capetown_whois.registry.net.za.capetown.Found.txt");
+
+            Assert.IsTrue(content.Length > 0);
+        }
+
+        [Test]
+        public void TestGetContentWhenNotFound()
+        {
+            var content = reader.GetContent("missing");
+
+            Assert.IsTrue(content.Length == 0);
+        }
+    }
+}
---
+++ Whois/Parsers/WhoisParser.cs
@@ -0,0 +1,56 @@
+﻿using Tokens;
+using Whois.Models;
+
+namespace Whois.Parsers
+{
+    /// <summary>
+    /// Parser to turn WHOIS server responses into <see cref="ParsedWhoisResponse"/>
+    /// objects.
+    /// </summary>
+    public class WhoisParser
+    {
+        private readonly TokenMatcher matcher;
+        private readonly ResourceReader reader;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="WhoisParser"/> class.
+        /// </summary>
+        public WhoisParser()
+        {
+            matcher = new TokenMatcher();
+            reader = new ResourceReader();
+        }
+
+        /// <summary>
+        /// Contains the registered templates
+        /// </summary>
+        public TemplateCollection Templates => matcher.Templates;
+
+        /// <summary>
+        /// Parses the WHOIS server response for the given server and TLD.
+        /// </summary>
+        public ParsedWhoisResponse Parse(string whoisServer, string tld, string content)
+        {
+            LoadServerTemplates(whoisServer, tld);
+
+            var result = matcher.Match<ParsedWhoisResponse>(content, new []{ whoisServer, tld });
+
+            return result.BestMatch?.Value;
+        }
+
+        private void LoadServerTemplates(string whoisServer, string tld)
+        {
+            // Check templates for this server/tld not already loaded
+            if (Templates.ContainsAllTags(whoisServer, tld)) return;
+
+            var templateNames = reader.GetNames(whoisServer, tld);
+
+            foreach (var templateName in templateNames)
+            {
+                var content = reader.GetContent(templateName);
+
+                matcher.RegisterTemplate(content);
+            }
+        }
+    }
+}
