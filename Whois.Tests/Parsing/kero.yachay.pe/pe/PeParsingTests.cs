e3a838606b8d2552e9976ad6082fd9b077529076
Chris Wood <chris@flipbit.co.uk>
2019-06-07 10:54:59 +0100

    Parse .pe domains

--- Whois.Tests/Parsing/kero.yachay.pe/pe/PeParsingTests.cs
+++ Whois.Tests/Parsing/kero.yachay.pe/pe/PeParsingTests.cs
@@ -25,6 +25,8 @@ namespace Whois.Parsing.Kero.Yachay.Pe.Pe
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Throttled, response.Status);
+            Assert.AreEqual(1, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
         }
 
         [Test]
@@ -35,6 +37,14 @@ namespace Whois.Parsing.Kero.Yachay.Pe.Pe
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(3, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("u34jedzcq.pe", response.DomainName);
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Not Registered", response.DomainStatus[0]);
         }
 
         [Test]
@@ -45,6 +55,22 @@ namespace Whois.Parsing.Kero.Yachay.Pe.Pe
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Inactive, response.Status);
+
+            Assert.AreEqual(7, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("zumba.pe", response.DomainName);
+
+            Assert.AreEqual("NIC .PE", response.Registrar.Name);
+
+            Assert.AreEqual("GRUPO ALBATROS SAC", response.Registrant.Name);
+
+            Assert.AreEqual("GRUPO ALBATROS SAC", response.AdminContact.Name);
+            Assert.AreEqual("jsotelo@galbatros.com", response.AdminContact.Email);
+
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Inactive", response.DomainStatus[0]);
         }
 
         [Test]
@@ -55,6 +81,28 @@ namespace Whois.Parsing.Kero.Yachay.Pe.Pe
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(11, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("google.pe", response.DomainName);
+
+            Assert.AreEqual("MarkMonitor Inc.", response.Registrar.Name);
+
+            Assert.AreEqual("google inc.", response.Registrant.Name);
+
+            Assert.AreEqual("MarkMonitor", response.AdminContact.Name);
+            Assert.AreEqual("ccops@markmonitor.com", response.AdminContact.Email);
+
+
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Active", response.DomainStatus[0]);        
         }
 
         [Test]
@@ -65,6 +113,27 @@ namespace Whois.Parsing.Kero.Yachay.Pe.Pe
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Suspended, response.Status);
+
+            Assert.AreEqual(11, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("bangladesh.pe", response.DomainName);
+
+            Assert.AreEqual("1API GmbH", response.Registrar.Name);
+
+            Assert.AreEqual("Ahmed Nitul", response.Registrant.Name);
+
+            Assert.AreEqual("Ahmed Nitul", response.AdminContact.Name);
+            Assert.AreEqual("ahmed@nitul.net", response.AdminContact.Email);
+
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.dnsimple.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.dnsimple.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.dnsimple.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.dnsimple.com", response.NameServers[3]);
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Suspended", response.DomainStatus[0]);
         }
     }
 }
