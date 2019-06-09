90d27bb31449402f3337204f15bdb01277b2ab53
Chris Wood <chris@flipbit.co.uk>
2019-06-09 18:06:08 +0100

    Add parsing of .au domains

--- Whois.Tests/Parsing/whois.audns.net.au/au/AuParsingTests.cs
+++ Whois.Tests/Parsing/whois.audns.net.au/au/AuParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -25,6 +26,34 @@ namespace Whois.Parsing.Whois.Audns.Net.Au.Au
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(15, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.audns.net.au/au/Found", response.TemplateName);
+
+            Assert.AreEqual("pinewood.com.au", response.DomainName);
+
+            Assert.AreEqual("Melbourne IT", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2010, 10, 11, 0, 0, 33), response.Updated);
+            Assert.AreEqual("ACN 120 562 905", response.Registrant.RegistryId);
+            Assert.AreEqual("PINEWOOD PROLAB PTY LTD", response.Registrant.Name);
+
+            Assert.AreEqual("Z116060879386417", response.AdminContact.RegistryId);
+            Assert.AreEqual("PETER TONOLI", response.AdminContact.Name);
+
+            Assert.AreEqual("Z116060879386417", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("PETER TONOLI", response.TechnicalContact.Name);
+
+
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.dreamhost.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.dreamhost.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.dreamhost.com", response.NameServers[2]);
+
+            Assert.AreEqual(2, response.DomainStatus.Count);
+            Assert.AreEqual("serverHold (Expired)", response.DomainStatus[0]);
+            Assert.AreEqual("serverUpdateProhibited (Expired)", response.DomainStatus[1]);
         }
 
         [Test]
@@ -35,6 +64,10 @@ namespace Whois.Parsing.Whois.Audns.Net.Au.Au
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(1, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.audns.net.au/au/NotFound", response.TemplateName);
         }
 
         [Test]
@@ -45,6 +78,35 @@ namespace Whois.Parsing.Whois.Audns.Net.Au.Au
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(16, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.audns.net.au/au/Found", response.TemplateName);
+
+            Assert.AreEqual("google.com.au", response.DomainName);
+
+
+            Assert.AreEqual(new DateTime(2014, 11, 5, 10, 35, 59), response.Updated);
+            Assert.AreEqual("Google INC", response.Registrant.Name);
+
+            Assert.AreEqual("MMR-122026", response.AdminContact.RegistryId);
+            Assert.AreEqual("Domain Administrator", response.AdminContact.Name);
+
+            Assert.AreEqual("MMR-87489", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.TechnicalContact.Name);
+
+
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(4, response.DomainStatus.Count);
+            Assert.AreEqual("clientDeleteProhibited", response.DomainStatus[0]);
+            Assert.AreEqual("clientUpdateProhibited", response.DomainStatus[1]);
+            Assert.AreEqual("serverDeleteProhibited (Protected by .auLOCKDOWN)", response.DomainStatus[2]);
+            Assert.AreEqual("serverUpdateProhibited (Protected by .auLOCKDOWN)", response.DomainStatus[3]);
         }
     }
 }
