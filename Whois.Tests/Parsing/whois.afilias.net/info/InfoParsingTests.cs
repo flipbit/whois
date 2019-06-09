a05d931dcc3298841eb9fb3c6dc086937b2b7138
Chris Wood <chris@flipbit.co.uk>
2019-06-09 05:08:41 +0100

    Add parsing of .info domains

--- Whois.Tests/Parsing/whois.afilias.net/info/InfoParsingTests.cs
+++ Whois.Tests/Parsing/whois.afilias.net/info/InfoParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -35,6 +36,87 @@ namespace Whois.Parsing.Whois.Afilias.Net.Info
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(60, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("google.info", response.DomainName);
+            Assert.AreEqual("D37288-LRMS", response.RegistryDomainId);
+
+            Assert.AreEqual("MarkMonitor Inc. (R151-LRMS)", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2001, 8, 1, 0, 57, 50), response.Updated);
+            Assert.AreEqual(new DateTime(2013, 6, 29, 10, 26, 18), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 8, 1, 0, 57, 50), response.Expiration);
+            Assert.AreEqual("mmr-32097", response.Registrant.RegistryId);
+            Assert.AreEqual("DNS Admin", response.Registrant.Name);
+            Assert.AreEqual("Google Inc.", response.Registrant.Organization);
+
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual(" 1600 Amphitheatre Parkway", response.Registrant.Address[0]);
+            Assert.AreEqual("Mountain View", response.Registrant.Address[1]);
+            Assert.AreEqual("CA", response.Registrant.Address[2]);
+            Assert.AreEqual("94043", response.Registrant.Address[3]);
+            Assert.AreEqual("US", response.Registrant.Address[4]);
+
+            Assert.AreEqual("+1.6506234000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual(" +1.6506188571", response.Registrant.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+            Assert.AreEqual("mmr-32097", response.AdminContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.AdminContact.Name);
+            Assert.AreEqual("Google Inc.", response.AdminContact.Organization);
+
+            Assert.AreEqual(5, response.AdminContact.Address.Count);
+            Assert.AreEqual(" 1600 Amphitheatre Parkway", response.AdminContact.Address[0]);
+            Assert.AreEqual("Mountain View", response.AdminContact.Address[1]);
+            Assert.AreEqual("CA", response.AdminContact.Address[2]);
+            Assert.AreEqual("94043", response.AdminContact.Address[3]);
+            Assert.AreEqual("US", response.AdminContact.Address[4]);
+
+            Assert.AreEqual(" +1.6506188571", response.AdminContact.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.AdminContact.Email);
+
+            Assert.AreEqual("mmr-32097", response.BillingContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.BillingContact.Name);
+            Assert.AreEqual("Google Inc.", response.BillingContact.Organization);
+
+            Assert.AreEqual(5, response.BillingContact.Address.Count);
+            Assert.AreEqual(" 1600 Amphitheatre Parkway", response.BillingContact.Address[0]);
+            Assert.AreEqual("Mountain View", response.BillingContact.Address[1]);
+            Assert.AreEqual("CA", response.BillingContact.Address[2]);
+            Assert.AreEqual("94043", response.BillingContact.Address[3]);
+            Assert.AreEqual("US", response.BillingContact.Address[4]);
+
+            Assert.AreEqual("+1.6506234000", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual(" +1.6506188571", response.BillingContact.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.BillingContact.Email);
+
+            Assert.AreEqual("mmr-32097", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.TechnicalContact.Name);
+            Assert.AreEqual("Google Inc.", response.TechnicalContact.Organization);
+
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual(" 1600 Amphitheatre Parkway", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Mountain View", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("94043", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("US", response.TechnicalContact.Address[3]);
+
+            Assert.AreEqual("+1.6506234000", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual(" +1.6506188571", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.TechnicalContact.Email);
+
+
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(3, response.DomainStatus.Count);
+            Assert.AreEqual("clientDeleteProhibited", response.DomainStatus[0]);
+            Assert.AreEqual("clientTransferProhibited", response.DomainStatus[1]);
+            Assert.AreEqual("clientUpdateProhibited", response.DomainStatus[2]);
         }
 
         [Test]
