4e36262f011ced886c3788bb1253a0bf17926f9c
Chris Wood <chris@flipbit.co.uk>
2019-06-09 09:29:12 +0100

    Add parsing of .lc domains

--- Whois.Tests/Parsing/whois.afilias-grs.info/lc/LcParsingTests.cs
+++ Whois.Tests/Parsing/whois.afilias-grs.info/lc/LcParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -25,6 +26,9 @@ namespace Whois.Parsing.Whois.Afilias.Grs.Info.Lc
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(1, response.FieldsParsed);
+            Assert.AreEqual("generic/tld/NotFound001", response.TemplateName);
         }
 
         [Test]
@@ -35,6 +39,63 @@ namespace Whois.Parsing.Whois.Afilias.Grs.Info.Lc
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(35, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found001", response.TemplateName);
+
+            Assert.AreEqual("nic.lc", response.DomainName);
+            Assert.AreEqual("D946482-LRCC", response.RegistryDomainId);
+
+            Assert.AreEqual("NicLc Registrar (R144-LRCC)", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2008, 12, 8, 19, 25, 9), response.Updated);
+            Assert.AreEqual(new DateTime(2002, 12, 8, 0, 0, 0), response.Registered);
+            Assert.AreEqual(new DateTime(2009, 12, 8, 0, 0, 0), response.Expiration);
+            Assert.AreEqual("LC-54921", response.Registrant.RegistryId);
+            Assert.AreEqual("Nic LC Admin", response.Registrant.Name);
+            Assert.AreEqual("Nic LC", response.Registrant.Organization);
+
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("Castries", response.Registrant.Address[0]);
+            Assert.AreEqual("Not Provided", response.Registrant.Address[1]);
+            Assert.AreEqual("Not Provided", response.Registrant.Address[2]);
+            Assert.AreEqual("LC", response.Registrant.Address[3]);
+
+            Assert.AreEqual("+758.4520220", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("nic@nic.lc", response.Registrant.Email);
+
+            Assert.AreEqual("LC-51893", response.AdminContact.RegistryId);
+            Assert.AreEqual("Nic LC Hostmaster", response.AdminContact.Name);
+            Assert.AreEqual("Nic LC", response.AdminContact.Organization);
+
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("Castries", response.AdminContact.Address[0]);
+            Assert.AreEqual("Not Provided", response.AdminContact.Address[1]);
+            Assert.AreEqual("Not Provided", response.AdminContact.Address[2]);
+            Assert.AreEqual("LC", response.AdminContact.Address[3]);
+
+            Assert.AreEqual("hostmaster@nic.lc", response.AdminContact.Email);
+
+            Assert.AreEqual("LC-53407", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Nic LC Technical", response.TechnicalContact.Name);
+            Assert.AreEqual("Nic LC", response.TechnicalContact.Organization);
+
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Castries", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Not Provided", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("LC", response.TechnicalContact.Address[2]);
+
+            Assert.AreEqual("+758.4520220", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("technical@nic.lc", response.TechnicalContact.Email);
+
+
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.nic.ag", response.NameServers[0]);
+            Assert.AreEqual("ns.patricklay.com", response.NameServers[1]);
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("OK", response.DomainStatus[0]);
         }
     }
 }
