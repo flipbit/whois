b34fbadcda1c146f298468098c1945f0d1f913b1
Chris Wood <chris@flipbit.co.uk>
2019-06-09 04:34:05 +0100

    Add parsing of .tv domains

--- Whois.Tests/Parsing/tvwhois.verisign-grs.com/tv/TvParsingTests.cs
+++ Whois.Tests/Parsing/tvwhois.verisign-grs.com/tv/TvParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -25,6 +26,20 @@ namespace Whois.Parsing.Tvwhois.Verisign.Grs.Com.Tv
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(7, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("set.tv", response.DomainName);
+
+            Assert.AreEqual(".TV RESERVED DOMAINS", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2010, 3, 18, 0, 0, 0), response.Updated);
+            Assert.AreEqual(new DateTime(2010, 3, 18, 0, 0, 0), response.Registered);
+            Assert.AreEqual(new DateTime(2011, 3, 18, 0, 0, 0), response.Expiration);
+
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("SERVER-XFER-PROHIBITED", response.DomainStatus[0]);
         }
 
         [Test]
@@ -35,6 +50,11 @@ namespace Whois.Parsing.Tvwhois.Verisign.Grs.Com.Tv
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(2, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("u34jedzcq.tv", response.DomainName);
         }
 
         [Test]
@@ -45,6 +65,36 @@ namespace Whois.Parsing.Tvwhois.Verisign.Grs.Com.Tv
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(21, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+
+            Assert.AreEqual("google.tv", response.DomainName);
+
+            Assert.AreEqual("MARKMONITOR INC.", response.Registrar.Name);
+            Assert.AreEqual("http://www.markmonitor.com", response.Registrar.Url);
+            Assert.AreEqual("whois.markmonitor.com", response.Registrar.WhoisServerUrl);
+            Assert.AreEqual("abusecomplaints@markmonitor.com", response.Registrar.AbuseEmail);
+            Assert.AreEqual("+1.2083895740", response.Registrar.AbuseTelephoneNumber);
+
+            Assert.AreEqual(new DateTime(2017, 7, 1, 10, 25, 47), response.Updated);
+            Assert.AreEqual(new DateTime(2002, 8, 2, 17, 43, 36), response.Registered);
+            Assert.AreEqual(new DateTime(2018, 8, 2, 17, 43, 36), response.Expiration);
+
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+
+            Assert.AreEqual(6, response.DomainStatus.Count);
+            Assert.AreEqual("clientDeleteProhibited", response.DomainStatus[0]);
+            Assert.AreEqual("clientTransferProhibited", response.DomainStatus[1]);
+            Assert.AreEqual("clientUpdateProhibited", response.DomainStatus[2]);
+            Assert.AreEqual("serverDeleteProhibited", response.DomainStatus[3]);
+            Assert.AreEqual("serverTransferProhibited", response.DomainStatus[4]);
+            Assert.AreEqual("serverUpdateProhibited", response.DomainStatus[5]);
+
+            Assert.AreEqual("unsigned", response.DnsSecStatus);
         }
     }
 }
