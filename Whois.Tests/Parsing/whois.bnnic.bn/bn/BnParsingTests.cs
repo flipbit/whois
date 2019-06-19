1d37ad6275596ef02d55470962cbda1e45453219
Chris Wood <chris@flipbit.co.uk>
2019-06-19 20:58:23 +0100

    Add parsing of .bn domains

--- Whois.Tests/Parsing/whois.bnnic.bn/bn/BnParsingTests.cs
+++ Whois.Tests/Parsing/whois.bnnic.bn/bn/BnParsingTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using Whois.Models;
 using Whois.Parsers;
 
@@ -25,6 +26,8 @@ namespace Whois.Parsing.Whois.Bnnic.Bn.Bn
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
         }
 
         [Test]
@@ -35,6 +38,34 @@ namespace Whois.Parsing.Whois.Bnnic.Bn.Bn
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(11, response.FieldsParsed);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.bnnic.bn/bn/Found", response.TemplateName);
+
+            Assert.AreEqual("telbru.com.bn", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("TELEKOM BRUNEI BERHAD", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2014, 12, 17, 18, 7, 42), response.Updated);
+            Assert.AreEqual(new DateTime(2014, 10, 7, 0, 0, 0), response.Registered);
+            Assert.AreEqual(new DateTime(2015, 10, 7, 0, 0, 0), response.Expiration);
+
+            // Registrant Details
+            Assert.AreEqual("BruNet| Telekom Brunei Berhad - (BNC875T)", response.Registrant.Name);
+
+             // AdminContact Details
+            Assert.AreEqual("BruNet| Telekom Brunei Berhad - (BNC875T)", response.AdminContact.Name);
+
+             // TechnicalContact Details
+            Assert.AreEqual("BruNet| Telekom Brunei Berhad - (BNC875T)", response.TechnicalContact.Name);
+            Assert.AreEqual("info@telbru.com.bn", response.TechnicalContact.Email);
+
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
         }
     }
 }
