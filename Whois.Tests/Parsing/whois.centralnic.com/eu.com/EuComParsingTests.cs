7108bf697f04759c19206e8c75637a67fa48e200
Chris Wood <chris@flipbit.co.uk>
2019-06-23 12:31:46 +0100

    Add parsing of .eu.com domains

--- Whois.Tests/Parsing/whois.centralnic.com/eu.com/EuComParsingTests.cs
+++ Whois.Tests/Parsing/whois.centralnic.com/eu.com/EuComParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -25,6 +26,11 @@ namespace Whois.Parsing.Whois.Centralnic.Com.EuCom
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.centralnic.com/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -35,6 +41,81 @@ namespace Whois.Parsing.Whois.Centralnic.Com.EuCom
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.centralnic.com/Found", response.TemplateName);
+
+            Assert.AreEqual("walkabout.eu.com", response.DomainName);
+            Assert.AreEqual("CNIC-DO85080", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("iTransact Ltd", response.Registrar.Name);
+            Assert.AreEqual("01223 700322", response.Registrar.AbuseTelephoneNumber);
+
+            Assert.AreEqual(new DateTime(2013, 8, 15, 12, 25, 43), response.Updated);
+            Assert.AreEqual(new DateTime(2001, 8, 14, 11, 14, 41), response.Registered);
+            Assert.AreEqual(new DateTime(2015, 8, 15, 0, 59, 59), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("H1045382", response.Registrant.RegistryId);
+            Assert.AreEqual("Regent Inns Plc", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("77 Muswell Hill", response.Registrant.Address[0]);
+            Assert.AreEqual("London", response.Registrant.Address[1]);
+            Assert.AreEqual("N10 3PJ", response.Registrant.Address[2]);
+            Assert.AreEqual("GB", response.Registrant.Address[3]);
+
+            Assert.AreEqual("+44.2083753155", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("john.boyle@regent-inns.plc.uk", response.Registrant.Email);
+
+
+             // AdminContact Details
+            Assert.AreEqual("H64717", response.AdminContact.RegistryId);
+            Assert.AreEqual("John Boyle", response.AdminContact.Name);
+            Assert.AreEqual("Regent Inns Plc", response.AdminContact.Organization);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("77 Muswell Hill", response.AdminContact.Address[0]);
+            Assert.AreEqual("London", response.AdminContact.Address[1]);
+            Assert.AreEqual("N10 3PJ", response.AdminContact.Address[2]);
+            Assert.AreEqual("GB", response.AdminContact.Address[3]);
+
+            Assert.AreEqual("+44.2083753155", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("john.boyle@regent-inns.plc.uk", response.AdminContact.Email);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("H126914", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Constantine Pagonis", response.TechnicalContact.Name);
+            Assert.AreEqual("iTransact Ltd", response.TechnicalContact.Organization);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("PO Box 430", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Cambridge", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("CB1 2WE", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("GB", response.TechnicalContact.Address[3]);
+
+            Assert.AreEqual("+44.1223700322", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("constantine@itransact.ltd.uk", response.TechnicalContact.Email);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns-1146.awsdns-15.org", response.NameServers[0]);
+            Assert.AreEqual("ns-1741.awsdns-25.co.uk", response.NameServers[1]);
+            Assert.AreEqual("ns-374.awsdns-46.com", response.NameServers[2]);
+            Assert.AreEqual("ns-914.awsdns-50.net", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual("Unsigned", response.DnsSecStatus);
+            Assert.AreEqual(40, response.FieldsParsed);
         }
     }
 }
