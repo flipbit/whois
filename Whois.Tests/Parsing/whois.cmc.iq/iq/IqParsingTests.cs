a83734d01ce4dbc4d3d1f703e100ef9a23583bb6
Chris Wood <chris@flipbit.co.uk>
2019-06-27 08:32:32 +0100

    Add parsing of .iq domains

--- Whois.Tests/Parsing/whois.cmc.iq/iq/IqParsingTests.cs
+++ Whois.Tests/Parsing/whois.cmc.iq/iq/IqParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Cmc.Iq.Iq
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class IqParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,17 @@ namespace Whois.Parsing.Whois.Cmc.Iq.Iq
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.cmc.iq/iq/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.iq", response.DomainName);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("No Object Found", response.DomainStatus[0]);
+
+            Assert.AreEqual(3, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +47,51 @@ namespace Whois.Parsing.Whois.Cmc.Iq.Iq
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.cmc.iq/iq/Found", response.TemplateName);
+
+            Assert.AreEqual("google.iq", response.DomainName);
+            Assert.AreEqual("895-CoCCA", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("CMC Registrar", response.Registrar.Name);
+            Assert.AreEqual("whois.cmc.iq", response.Registrar.WhoisServerUrl);
+
+            Assert.AreEqual(new DateTime(2013, 09, 29, 05, 19, 04, 997, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2010, 10, 03, 21, 00, 00, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 10, 02, 21, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("1443-cmc", response.Registrant.RegistryId);
+            Assert.AreEqual("Dr.akraym al-hak baker", response.Registrant.Name);
+            Assert.AreEqual("+964.7901790160", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("bl-yoban@yahoo.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("al-yarmuk", response.Registrant.Address[0]);
+            Assert.AreEqual("baghdad", response.Registrant.Address[1]);
+            Assert.AreEqual("IQ", response.Registrant.Address[2]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("2640-cmc", response.AdminContact.RegistryId);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual("unsigned", response.DnsSecStatus);
+            Assert.AreEqual(23, response.FieldsParsed);
         }
     }
 }
