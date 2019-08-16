6afa715735ce0999075aa43d58d6ac2a2da87358
Chris Wood <chris@flipbit.co.uk>
2019-08-16 05:19:13 +0100

    Add partial parsing of .ie domains

--- Whois.Tests/Parsing/whois.domainregistry.ie/ie/IeParsingTests.cs
+++ Whois.Tests/Parsing/whois.domainregistry.ie/ie/IeParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class IeParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -19,13 +19,22 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
         }
 
         [Test]
-        public void Test_found()
+        public void Test_reserved()
         {
-            var sample = SampleReader.Read("whois.domainregistry.ie", "ie", "found.txt");
+            var sample = SampleReader.Read("whois.domainregistry.ie", "ie", "reserved.txt");
             var response = parser.Parse("whois.domainregistry.ie", "ie", sample);
 
             Assert.Greater(sample.Length, 0);
-            Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+            Assert.AreEqual(WhoisResponseStatus.Reserved, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/Reserved", response.TemplateName);
+
+            Assert.AreEqual("peter.ie", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2012, 04, 17, 00, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+            Assert.AreEqual(3, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +45,29 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+            
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/Found02", response.TemplateName);
+
+            Assert.AreEqual("rte.ie", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2012, 03, 31, 00, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("RTE Commercial Enterprises Limited", response.Registrant.Name);
+
+             // AdminContact Details
+            Assert.AreEqual("JL241-IEDR", response.AdminContact.RegistryId);
+
+             // TechnicalContact Details
+            Assert.AreEqual("JM474-IEDR", response.TechnicalContact.RegistryId);
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns3.rte.ie", response.NameServers[0]);
+            Assert.AreEqual("ns4.rte.ie", response.NameServers[1]);
+
+            Assert.AreEqual(8, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +78,34 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/Found", response.TemplateName);
+
+            Assert.AreEqual("tcd.ie", response.DomainName);
+
+            Assert.AreEqual(new DateTime(1999, 08, 24, 00, 00, 00, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2013, 08, 24, 00, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("University of Dublin Trinity College", response.Registrant.Name);
+
+             // AdminContact Details
+            Assert.AreEqual("AAB502-IEDR", response.AdminContact.RegistryId);
+
+             // TechnicalContact Details
+            Assert.AreEqual("KG37-IEDR", response.TechnicalContact.RegistryId);
+
+            // Nameservers
+            Assert.AreEqual(6, response.NameServers.Count);
+            Assert.AreEqual("ns1.tcd.ie", response.NameServers[0]);
+            Assert.AreEqual("ns2.tcd.ie", response.NameServers[1]);
+            Assert.AreEqual("ns.maths.tcd.ie", response.NameServers[2]);
+            Assert.AreEqual("sec2.authdns.ripe.net", response.NameServers[3]);
+            Assert.AreEqual("ns.tcd.ie", response.NameServers[4]);
+            Assert.AreEqual("auth-ns1.ucd.ie", response.NameServers[5]);
+
+            Assert.AreEqual(13, response.FieldsParsed);
         }
 
         [Test]
@@ -56,6 +116,33 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/Found02", response.TemplateName);
+
+            Assert.AreEqual("dns.ie", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2021, 02, 20, 00, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("Irish Domains Ltd", response.Registrant.Name);
+
+             // AdminContact Details
+            Assert.AreEqual("CM417-IEDR", response.AdminContact.RegistryId);
+
+             // TechnicalContact Details
+            Assert.AreEqual("TDI2-IEDR", response.TechnicalContact.RegistryId);
+
+            // Nameservers
+            Assert.AreEqual(6, response.NameServers.Count);
+            Assert.AreEqual("ns1.dns.ie", response.NameServers[0]);
+            Assert.AreEqual("ns2.dns.ie", response.NameServers[1]);
+            Assert.AreEqual("ns3.dns.ie", response.NameServers[2]);
+            Assert.AreEqual("ns4.dns.ie", response.NameServers[3]);
+            Assert.AreEqual("ns5.dns.ie", response.NameServers[4]);
+            Assert.AreEqual("ns6.dns.ie", response.NameServers[5]);
+
+            Assert.AreEqual(12, response.FieldsParsed);
         }
 
         [Test]
@@ -66,6 +153,13 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.ie", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -76,6 +170,35 @@ namespace Whois.Parsing.Whois.Domainregistry.Ie.Ie
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domainregistry.ie/ie/Found", response.TemplateName);
+
+            Assert.AreEqual("google.ie", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2002, 03, 21, 00, 00, 00, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2015, 03, 21, 00, 00, 00, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("Google, Inc", response.Registrant.Name);
+
+             // AdminContact Details
+            Assert.AreEqual("AAV410-IEDR", response.AdminContact.RegistryId);
+
+             // TechnicalContact Details
+            Assert.AreEqual("CCA7-IEDR", response.TechnicalContact.RegistryId);
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Active", response.DomainStatus[0]);
+
+            Assert.AreEqual(11, response.FieldsParsed);
         }
     }
 }
