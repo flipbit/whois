5f184ab02c45eee37a235dca950018eb9aecc859
Chris Wood <chris@flipbit.co.uk>
2019-08-16 05:40:18 +0100

    Add parsing of .lt domains

--- Whois.Tests/Parsing/whois.domreg.lt/lt/LtParsingTests.cs
+++ Whois.Tests/Parsing/whois.domreg.lt/lt/LtParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Domreg.Lt.Lt
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class LtParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,27 @@ namespace Whois.Parsing.Whois.Domreg.Lt.Lt
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domreg.lt/lt/Found", response.TemplateName);
+
+            Assert.AreEqual("serveriai.lt", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual(@"UAB ""Interneto vizija""", response.Registrar.Name);
+            Assert.AreEqual("http://www.iv.lt/", response.Registrar.Url);
+
+            Assert.AreEqual(new DateTime(2003, 11, 17, 00, 00, 00, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual(@"UAB ""Interneto vizija""", response.Registrant.Organization);
+            Assert.AreEqual("hostmaster@iv.lt", response.Registrant.Email);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("registered", response.DomainStatus[0]);
+
+            Assert.AreEqual(8, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +57,13 @@ namespace Whois.Parsing.Whois.Domreg.Lt.Lt
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domreg.lt/lt/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.lt", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +74,32 @@ namespace Whois.Parsing.Whois.Domreg.Lt.Lt
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.domreg.lt/lt/Found", response.TemplateName);
+
+            Assert.AreEqual("google.lt", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("MarkMonitor, Inc.", response.Registrar.Name);
+            Assert.AreEqual("http://www.markmonitor.com", response.Registrar.Url);
+
+            Assert.AreEqual(new DateTime(1999, 06, 06, 00, 00, 00, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("registered", response.DomainStatus[0]);
+
+            Assert.AreEqual(10, response.FieldsParsed);
         }
     }
 }
