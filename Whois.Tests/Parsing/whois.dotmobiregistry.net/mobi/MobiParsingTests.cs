23bf3e1c3c668f3390ba7611b794ca0e3102c827
Chris Wood <chris@flipbit.co.uk>
2019-08-17 04:42:32 +0100

    Add parsing of .mobi domains

--- Whois.Tests/Parsing/whois.dotmobiregistry.net/mobi/MobiParsingTests.cs
+++ Whois.Tests/Parsing/whois.dotmobiregistry.net/mobi/MobiParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Dotmobiregistry.Net.Mobi
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class MobiParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Dotmobiregistry.Net.Mobi
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound001", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,78 @@ namespace Whois.Parsing.Whois.Dotmobiregistry.Net.Mobi
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found001", response.TemplateName);
+
+            Assert.AreEqual("google.mobi", response.DomainName);
+            Assert.AreEqual("D117-MOBI", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("mTLD Mobile Top Level Domain (4000002)", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2013, 04, 09, 09, 24, 02, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2006, 05, 11, 21, 08, 42, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 05, 11, 21, 08, 42, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("mmr-14290820", response.Registrant.RegistryId);
+            Assert.AreEqual("DNS Admin", response.Registrant.Name);
+            Assert.AreEqual("Google Inc.", response.Registrant.Organization);
+            Assert.AreEqual("+1.6506234000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("Mountain View", response.Registrant.Address[0]);
+            Assert.AreEqual("CA", response.Registrant.Address[1]);
+            Assert.AreEqual("94043", response.Registrant.Address[2]);
+            Assert.AreEqual("US", response.Registrant.Address[3]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("mmr-14290820", response.AdminContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.AdminContact.Name);
+            Assert.AreEqual("Google Inc.", response.AdminContact.Organization);
+            Assert.AreEqual("+1.6506234000", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("dns-admin@google.com", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("Mountain View", response.AdminContact.Address[0]);
+            Assert.AreEqual("CA", response.AdminContact.Address[1]);
+            Assert.AreEqual("94043", response.AdminContact.Address[2]);
+            Assert.AreEqual("US", response.AdminContact.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("mmr-14290820", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("DNS Admin", response.TechnicalContact.Name);
+            Assert.AreEqual("Google Inc.", response.TechnicalContact.Organization);
+            Assert.AreEqual("+1.6506234000", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("dns-admin@google.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Mountain View", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("94043", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("US", response.TechnicalContact.Address[2]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(3, response.DomainStatus.Count);
+            Assert.AreEqual("CLIENT DELETE PROHIBITED", response.DomainStatus[0]);
+            Assert.AreEqual("CLIENT TRANSFER PROHIBITED", response.DomainStatus[1]);
+            Assert.AreEqual("CLIENT UPDATE PROHIBITED", response.DomainStatus[2]);
+
+            Assert.AreEqual(44, response.FieldsParsed);
         }
     }
 }
