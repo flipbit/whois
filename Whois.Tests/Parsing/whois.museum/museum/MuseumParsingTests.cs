0564e5a230b38aa4781944526daa8b87ce36503a
Chris Wood <chris@flipbit.co.uk>
2019-09-01 10:21:00 +0100

    Add parsing of .museum domains

--- Whois.Tests/Parsing/whois.museum/museum/MuseumParsingTests.cs
+++ Whois.Tests/Parsing/whois.museum/museum/MuseumParsingTests.cs
@@ -5,7 +5,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Museum.Museum
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class MuseumParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +25,13 @@ namespace Whois.Parsing.Whois.Museum.Museum
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.museum/museum/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.museum", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +42,79 @@ namespace Whois.Parsing.Whois.Museum.Museum
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found02", response.TemplateName);
+
+            Assert.AreEqual("musedoma.museum", response.DomainName);
+            Assert.AreEqual("D778-MUSEUM", response.RegistryDomainId);
+
+
+             // Registrant Details
+            Assert.AreEqual("AC727-MUSEUM", response.Registrant.RegistryId);
+            Assert.AreEqual("n/a", response.Registrant.Name);
+            Assert.AreEqual("Museum Domain Management Association", response.Registrant.Organization);
+            Assert.AreEqual("ck@nrm.se", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("Frescativaegen 40", response.Registrant.Address[0]);
+            Assert.AreEqual("Stockholm", response.Registrant.Address[1]);
+            Assert.AreEqual("104 05", response.Registrant.Address[2]);
+            Assert.AreEqual("SE", response.Registrant.Address[3]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("C728-MUSEUM", response.AdminContact.RegistryId);
+            Assert.AreEqual("Cary Karp", response.AdminContact.Name);
+            Assert.AreEqual("Museum Domain Management Association", response.AdminContact.Organization);
+            Assert.AreEqual("ck@nic.museum", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("Frescativaegen 40", response.AdminContact.Address[0]);
+            Assert.AreEqual("Stockholm", response.AdminContact.Address[1]);
+            Assert.AreEqual("104 05", response.AdminContact.Address[2]);
+            Assert.AreEqual("SE", response.AdminContact.Address[3]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("C728-MUSEUM", response.BillingContact.RegistryId);
+            Assert.AreEqual("Cary Karp", response.BillingContact.Name);
+            Assert.AreEqual("Museum Domain Management Association", response.BillingContact.Organization);
+            Assert.AreEqual("ck@nic.museum", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(4, response.BillingContact.Address.Count);
+            Assert.AreEqual("Frescativaegen 40", response.BillingContact.Address[0]);
+            Assert.AreEqual("Stockholm", response.BillingContact.Address[1]);
+            Assert.AreEqual("104 05", response.BillingContact.Address[2]);
+            Assert.AreEqual("SE", response.BillingContact.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("C728-MUSEUM", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Cary Karp", response.TechnicalContact.Name);
+            Assert.AreEqual("Museum Domain Management Association", response.TechnicalContact.Organization);
+            Assert.AreEqual("ck@nic.museum", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Frescativaegen 40", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Stockholm", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("104 05", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("SE", response.TechnicalContact.Address[3]);
+
+
+            // Nameservers
+            Assert.AreEqual(1, response.NameServers.Count);
+            Assert.AreEqual("nic.frd.se", response.NameServers[0]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual(37, response.FieldsParsed);
         }
     }
 }
