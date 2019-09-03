d06e4962d1ba42a295af352d1afe6b865d5288b7
Chris Wood <chris@flipbit.co.uk>
2019-09-03 08:15:01 +0100

    Add parsing of .sb domains

--- Whois.Tests/Parsing/whois.nic.net.sb/sb/SbParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.net.sb/sb/SbParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Net.Sb.Sb
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class SbParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Nic.Net.Sb.Sb
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound005", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.sb", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,92 @@ namespace Whois.Parsing.Whois.Nic.Net.Sb.Sb
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found02", response.TemplateName);
+
+            Assert.AreEqual("baidu.com.sb", response.DomainName);
+            Assert.AreEqual("404765-CoCCA", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("Key-Systems", response.Registrar.Name);
+            Assert.AreEqual("http://www.key-systems.net", response.Registrar.Url);
+            Assert.AreEqual("info@key-systems.net", response.Registrar.AbuseEmail);
+            Assert.AreEqual("+49 (0)68949396850", response.Registrar.AbuseTelephoneNumber);
+
+            Assert.AreEqual(new DateTime(2012, 02, 26, 05, 08, 41, 045, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2010, 03, 27, 04, 29, 19, 249, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2013, 03, 27, 04, 29, 19, 346, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("419751-CoCCA", response.Registrant.RegistryId);
+            Assert.AreEqual("long dian", response.Registrant.Name);
+            Assert.AreEqual("999.cn.vc", response.Registrant.Organization);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("shannxi", response.Registrant.Address[0]);
+            Assert.AreEqual("xian", response.Registrant.Address[1]);
+            Assert.AreEqual("710000", response.Registrant.Address[2]);
+            Assert.AreEqual("CN", response.Registrant.Address[3]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("419751-CoCCA", response.AdminContact.RegistryId);
+            Assert.AreEqual("long dian", response.AdminContact.Name);
+            Assert.AreEqual("999.cn.vc", response.AdminContact.Organization);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("shannxi", response.AdminContact.Address[0]);
+            Assert.AreEqual("xian", response.AdminContact.Address[1]);
+            Assert.AreEqual("710000", response.AdminContact.Address[2]);
+            Assert.AreEqual("CN", response.AdminContact.Address[3]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("419751-CoCCA", response.BillingContact.RegistryId);
+            Assert.AreEqual("long dian", response.BillingContact.Name);
+            Assert.AreEqual("999.cn.vc", response.BillingContact.Organization);
+            Assert.AreEqual("+1.123456789", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual("+1.123456789", response.BillingContact.FaxNumber);
+            Assert.AreEqual("hostcn@gmail.com", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(4, response.BillingContact.Address.Count);
+            Assert.AreEqual("shannxi", response.BillingContact.Address[0]);
+            Assert.AreEqual("xian", response.BillingContact.Address[1]);
+            Assert.AreEqual("710000", response.BillingContact.Address[2]);
+            Assert.AreEqual("CN", response.BillingContact.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("419751-CoCCA", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("long dian", response.TechnicalContact.Name);
+            Assert.AreEqual("999.cn.vc", response.TechnicalContact.Organization);
+            Assert.AreEqual("+1.123456789", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("+1.123456789", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("hostcn@gmail.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("shannxi", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("xian", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("710000", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("CN", response.TechnicalContact.Address[3]);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("f1g1ns1.dnspod.net", response.NameServers[0]);
+            Assert.AreEqual("f1g1ns2.dnspod.net", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual("unsigned", response.DnsSecStatus);
+            Assert.AreEqual(48, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +139,13 @@ namespace Whois.Parsing.Whois.Nic.Net.Sb.Sb
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound005", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.sb", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -56,6 +156,92 @@ namespace Whois.Parsing.Whois.Nic.Net.Sb.Sb
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found02", response.TemplateName);
+
+            Assert.AreEqual("baidu.com.sb", response.DomainName);
+            Assert.AreEqual("404765-CoCCA", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("Key-Systems", response.Registrar.Name);
+            Assert.AreEqual("http://www.key-systems.net", response.Registrar.Url);
+            Assert.AreEqual("info@key-systems.net", response.Registrar.AbuseEmail);
+            Assert.AreEqual("+49 (0)68949396850", response.Registrar.AbuseTelephoneNumber);
+
+            Assert.AreEqual(new DateTime(2012, 02, 26, 05, 08, 41, 045, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2010, 03, 27, 04, 29, 19, 249, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2013, 03, 27, 04, 29, 19, 346, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("419751-CoCCA", response.Registrant.RegistryId);
+            Assert.AreEqual("long dian", response.Registrant.Name);
+            Assert.AreEqual("999.cn.vc", response.Registrant.Organization);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("shannxi", response.Registrant.Address[0]);
+            Assert.AreEqual("xian", response.Registrant.Address[1]);
+            Assert.AreEqual("710000", response.Registrant.Address[2]);
+            Assert.AreEqual("CN", response.Registrant.Address[3]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("419751-CoCCA", response.AdminContact.RegistryId);
+            Assert.AreEqual("long dian", response.AdminContact.Name);
+            Assert.AreEqual("999.cn.vc", response.AdminContact.Organization);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("shannxi", response.AdminContact.Address[0]);
+            Assert.AreEqual("xian", response.AdminContact.Address[1]);
+            Assert.AreEqual("710000", response.AdminContact.Address[2]);
+            Assert.AreEqual("CN", response.AdminContact.Address[3]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("419751-CoCCA", response.BillingContact.RegistryId);
+            Assert.AreEqual("long dian", response.BillingContact.Name);
+            Assert.AreEqual("999.cn.vc", response.BillingContact.Organization);
+            Assert.AreEqual("+1.123456789", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual("+1.123456789", response.BillingContact.FaxNumber);
+            Assert.AreEqual("hostcn@gmail.com", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(4, response.BillingContact.Address.Count);
+            Assert.AreEqual("shannxi", response.BillingContact.Address[0]);
+            Assert.AreEqual("xian", response.BillingContact.Address[1]);
+            Assert.AreEqual("710000", response.BillingContact.Address[2]);
+            Assert.AreEqual("CN", response.BillingContact.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("419751-CoCCA", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("long dian", response.TechnicalContact.Name);
+            Assert.AreEqual("999.cn.vc", response.TechnicalContact.Organization);
+            Assert.AreEqual("+1.123456789", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("+1.123456789", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("hostcn@gmail.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("shannxi", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("xian", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("710000", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("CN", response.TechnicalContact.Address[3]);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("f1g1ns1.dnspod.net", response.NameServers[0]);
+            Assert.AreEqual("f1g1ns2.dnspod.net", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual("unsigned", response.DnsSecStatus);
+            Assert.AreEqual(48, response.FieldsParsed);
         }
     }
 }
