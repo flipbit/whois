3b0bc33df4709a44081b7458f784184c9601b069
Chris Wood <chris@flipbit.co.uk>
2019-09-03 08:42:55 +0100

    Add parsing of .xxx domains

--- Whois.Tests/Parsing/whois.nic.xxx/xxx/XxxParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.xxx/xxx/XxxParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Xxx.Xxx
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class XxxParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Nic.Xxx.Xxx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound001", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,82 @@ namespace Whois.Parsing.Whois.Nic.Xxx.Xxx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found001", response.TemplateName);
+
+            Assert.AreEqual("masala.xxx", response.DomainName);
+            Assert.AreEqual("D130773-XXX", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("Mesh Digital Limited (R3228-XXX)", response.Registrar.Name);
+            Assert.AreEqual(" 1390", response.Registrar.IanaId);
+
+            Assert.AreEqual(new DateTime(2013, 12, 02, 22, 20, 04, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2011, 12, 01, 01, 37, 55, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 12, 01, 01, 37, 55, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("MNT5BB23000590", response.Registrant.RegistryId);
+            Assert.AreEqual("Domainmonster.com Privacy Service", response.Registrant.Name);
+            Assert.AreEqual("Identity Protect Limited", response.Registrant.Organization);
+            Assert.AreEqual("+44.1483307527", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+44.1483304031", response.Registrant.FaxNumber);
+            Assert.AreEqual("masala.xxx@privatemonster.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual("PO Box 795", response.Registrant.Address[0]);
+            Assert.AreEqual("Godalming", response.Registrant.Address[1]);
+            Assert.AreEqual("Surrey", response.Registrant.Address[2]);
+            Assert.AreEqual("GU7 9GA", response.Registrant.Address[3]);
+            Assert.AreEqual("GB", response.Registrant.Address[4]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("MNT5BB23000590", response.AdminContact.RegistryId);
+            Assert.AreEqual("Domainmonster.com Privacy Service", response.AdminContact.Name);
+            Assert.AreEqual("Identity Protect Limited", response.AdminContact.Organization);
+            Assert.AreEqual("+44.1483307527", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("+44.1483304031", response.AdminContact.FaxNumber);
+            Assert.AreEqual("masala.xxx@privatemonster.com", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(5, response.AdminContact.Address.Count);
+            Assert.AreEqual("PO Box 795", response.AdminContact.Address[0]);
+            Assert.AreEqual("Godalming", response.AdminContact.Address[1]);
+            Assert.AreEqual("Surrey", response.AdminContact.Address[2]);
+            Assert.AreEqual("GU7 9GA", response.AdminContact.Address[3]);
+            Assert.AreEqual("GB", response.AdminContact.Address[4]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("MNT5BB23000590", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Domainmonster.com Privacy Service", response.TechnicalContact.Name);
+            Assert.AreEqual("Identity Protect Limited", response.TechnicalContact.Organization);
+            Assert.AreEqual("+44.1483307527", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("+44.1483304031", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("masala.xxx@privatemonster.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(5, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("PO Box 795", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Godalming", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("Surrey", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("GU7 9GA", response.TechnicalContact.Address[3]);
+            Assert.AreEqual("GB", response.TechnicalContact.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns39.domaincontrol.com", response.NameServers[0]);
+            Assert.AreEqual("ns40.domaincontrol.com", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual(44, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +127,11 @@ namespace Whois.Parsing.Whois.Nic.Xxx.Xxx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Reserved, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.xxx/xxx/Reserved", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
     }
 }
