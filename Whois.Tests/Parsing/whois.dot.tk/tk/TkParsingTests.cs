252727afddb61e6f8b62cc0ae1d16da55de0d54d
Chris Wood <chris@flipbit.co.uk>
2019-09-04 08:28:04 +0100

    Add parsing of .tk domains

--- Whois.Tests/Parsing/whois.dot.tk/tk/TkParsingTests.cs
+++ Whois.Tests/Parsing/whois.dot.tk/tk/TkParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Dot.Tk.Tk
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class TkParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Dot.Tk.Tk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.dot.tk/tk/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,38 @@ namespace Whois.Parsing.Whois.Dot.Tk.Tk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.dot.tk/tk/Found", response.TemplateName);
+
+            Assert.AreEqual("google.tk", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2001, 12, 18, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2012, 03, 02, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("eMarkmonitor Inc", response.Registrant.Organization);
+            Assert.AreEqual("+1 208-3895740", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1 208-3895799", response.Registrant.FaxNumber);
+            Assert.AreEqual("ccopsbilling@markmonitor.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual("Ccops Center", response.Registrant.Address[0]);
+            Assert.AreEqual("PMB 155, 10400 Overland Road", response.Registrant.Address[1]);
+            Assert.AreEqual("83709  Boise", response.Registrant.Address[2]);
+            Assert.AreEqual("Idaho", response.Registrant.Address[3]);
+            Assert.AreEqual("U.S.A.", response.Registrant.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(17, response.FieldsParsed);
         }
     }
 }
