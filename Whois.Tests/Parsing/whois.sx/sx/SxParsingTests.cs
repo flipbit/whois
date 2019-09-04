512a679a22c5d2842b05aa01544c63ed118251af
Chris Wood <chris@flipbit.co.uk>
2019-09-04 05:39:20 +0100

    Add parsing of .sx domains

--- Whois.Tests/Parsing/whois.sx/sx/SxParsingTests.cs
+++ Whois.Tests/Parsing/whois.sx/sx/SxParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Sx.Sx
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class SxParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -25,7 +25,14 @@ namespace Whois.Parsing.Whois.Sx.Sx
             var response = parser.Parse("whois.sx", "sx", sample);
 
             Assert.Greater(sample.Length, 0);
-            Assert.AreEqual(WhoisResponseStatus.Other, response.Status);
+            Assert.AreEqual(WhoisResponseStatus.Unavailable, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sx/sx/Unavailable", response.TemplateName);
+
+            Assert.AreEqual("domain.sx", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,13 @@ namespace Whois.Parsing.Whois.Sx.Sx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sx/sx/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.sx", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +60,74 @@ namespace Whois.Parsing.Whois.Sx.Sx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found02", response.TemplateName);
+
+            Assert.AreEqual("whois.sx", response.DomainName);
+            Assert.AreEqual("d5-sx", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("SX Registry O", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2013, 02, 25, 16, 50, 39, 204, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2011, 12, 09, 14, 07, 22, 794, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2022, 12, 09, 14, 07, 22, 794, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("C65", response.Registrant.RegistryId);
+            Assert.AreEqual("SX Registry SA administrator", response.Registrant.Name);
+            Assert.AreEqual("SX Registry SA", response.Registrant.Organization);
+            Assert.AreEqual("registry@registry.sx", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("2, rue Léon Laval", response.Registrant.Address[0]);
+            Assert.AreEqual("Leudelange", response.Registrant.Address[1]);
+            Assert.AreEqual("L3372", response.Registrant.Address[2]);
+            Assert.AreEqual("LUXEMBOURG", response.Registrant.Address[3]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("C65", response.AdminContact.RegistryId);
+            Assert.AreEqual("SX Registry SA administrator", response.AdminContact.Name);
+            Assert.AreEqual("SX Registry SA", response.AdminContact.Organization);
+            Assert.AreEqual("registry@registry.sx", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(4, response.AdminContact.Address.Count);
+            Assert.AreEqual("2, rue Léon Laval", response.AdminContact.Address[0]);
+            Assert.AreEqual("Leudelange", response.AdminContact.Address[1]);
+            Assert.AreEqual("L3372", response.AdminContact.Address[2]);
+            Assert.AreEqual("LUXEMBOURG", response.AdminContact.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("C65", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("SX Registry SA administrator", response.TechnicalContact.Name);
+            Assert.AreEqual("SX Registry SA", response.TechnicalContact.Organization);
+            Assert.AreEqual("registry@registry.sx", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("2, rue Léon Laval", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Leudelange", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("L3372", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("LUXEMBOURG", response.TechnicalContact.Address[3]);
+
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("a.ns.sx", response.NameServers[0]);
+            Assert.AreEqual("b.ns.sx", response.NameServers[1]);
+            Assert.AreEqual("c.ns.sx", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual("signedDelegation", response.DnsSecStatus);
+            Assert.AreEqual(36, response.FieldsParsed);
         }
 
         [Test]
@@ -56,6 +138,13 @@ namespace Whois.Parsing.Whois.Sx.Sx
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Unavailable, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sx/sx/Unavailable", response.TemplateName);
+
+            Assert.AreEqual("domain.sx", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
     }
 }
