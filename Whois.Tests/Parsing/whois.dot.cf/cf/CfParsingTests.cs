f851bcb3cd0699574d9255623a9f8e24171d29d3
Chris Wood <chris@flipbit.co.uk>
2019-09-04 05:55:25 +0100

    Add parsing of .cf domains

--- Whois.Tests/Parsing/whois.dot.cf/cf/CfParsingTests.cs
+++ Whois.Tests/Parsing/whois.dot.cf/cf/CfParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Dot.Cf.Cf
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class CfParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,87 @@ namespace Whois.Parsing.Whois.Dot.Cf.Cf
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.dot.cf/cf/Found", response.TemplateName);
+
+            Assert.AreEqual("dot.cf", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2013, 03, 28, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.Registrant.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.Registrant.Organization);
+            Assert.AreEqual("20-5315726", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.Registrant.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.Registrant.Address[0]);
+            Assert.AreEqual("1016DT", response.Registrant.Address[1]);
+            Assert.AreEqual("Amsterdam", response.Registrant.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.Registrant.Address[3]);
+            Assert.AreEqual("Netherlands", response.Registrant.Address[4]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.AdminContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.AdminContact.Organization);
+            Assert.AreEqual("20-5315726", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.AdminContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(5, response.AdminContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.AdminContact.Address[0]);
+            Assert.AreEqual("1016DT", response.AdminContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.AdminContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.AdminContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.AdminContact.Address[4]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.BillingContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.BillingContact.Organization);
+            Assert.AreEqual("20-5315726", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.BillingContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(5, response.BillingContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.BillingContact.Address[0]);
+            Assert.AreEqual("1016DT", response.BillingContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.BillingContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.BillingContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.BillingContact.Address[4]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.TechnicalContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.TechnicalContact.Organization);
+            Assert.AreEqual("20-5315726", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(5, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("1016DT", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.TechnicalContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.TechnicalContact.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(5, response.NameServers.Count);
+            Assert.AreEqual("dns5.nettica.com", response.NameServers[0]);
+            Assert.AreEqual("dns1.nettica.com", response.NameServers[1]);
+            Assert.AreEqual("dns2.nettica.com", response.NameServers[2]);
+            Assert.AreEqual("dns3.nettica.com", response.NameServers[3]);
+            Assert.AreEqual("dns4.nettica.com", response.NameServers[4]);
+
+            Assert.AreEqual(48, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +117,11 @@ namespace Whois.Parsing.Whois.Dot.Cf.Cf
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.dot.cf/cf/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +132,88 @@ namespace Whois.Parsing.Whois.Dot.Cf.Cf
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.dot.cf/cf/Found", response.TemplateName);
+
+            Assert.AreEqual("dot.cf", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2013, 03, 28, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.Registrant.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.Registrant.Organization);
+            Assert.AreEqual("20-5315726", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.Registrant.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.Registrant.Address[0]);
+            Assert.AreEqual("1016DT", response.Registrant.Address[1]);
+            Assert.AreEqual("Amsterdam", response.Registrant.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.Registrant.Address[3]);
+            Assert.AreEqual("Netherlands", response.Registrant.Address[4]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.AdminContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.AdminContact.Organization);
+            Assert.AreEqual("20-5315726", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.AdminContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(5, response.AdminContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.AdminContact.Address[0]);
+            Assert.AreEqual("1016DT", response.AdminContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.AdminContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.AdminContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.AdminContact.Address[4]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.BillingContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.BillingContact.Organization);
+            Assert.AreEqual("20-5315726", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.BillingContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(5, response.BillingContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.BillingContact.Address[0]);
+            Assert.AreEqual("1016DT", response.BillingContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.BillingContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.BillingContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.BillingContact.Address[4]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("Mr Joost  Zuurbier", response.TechnicalContact.Name);
+            Assert.AreEqual("Centrafrique TLD B.V.", response.TechnicalContact.Organization);
+            Assert.AreEqual("20-5315726", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("20-5315721", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("info@centrafriquetld.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(5, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Keizersgracht 213", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("1016DT", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("Amsterdam", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("Noord-Holland", response.TechnicalContact.Address[3]);
+            Assert.AreEqual("Netherlands", response.TechnicalContact.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(5, response.NameServers.Count);
+            Assert.AreEqual("dns5.nettica.com", response.NameServers[0]);
+            Assert.AreEqual("dns1.nettica.com", response.NameServers[1]);
+            Assert.AreEqual("dns2.nettica.com", response.NameServers[2]);
+            Assert.AreEqual("dns3.nettica.com", response.NameServers[3]);
+            Assert.AreEqual("dns4.nettica.com", response.NameServers[4]);
+
+            Assert.AreEqual(48, response.FieldsParsed);
         }
     }
 }
