ae442c08f6ba1cfa9a7d86ac3462ab5f8aa37f2f
Chris Wood <chris@flipbit.co.uk>
2019-09-05 08:11:16 +0100

    Add parsing of .ee domains

--- Whois.Tests/Parsing/whois.eenet.ee/ee/EeParsingTests.cs
+++ Whois.Tests/Parsing/whois.eenet.ee/ee/EeParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Eenet.Ee.Ee
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class EeParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Eenet.Ee.Ee
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.eenet.ee/ee/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.ee", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,42 @@ namespace Whois.Parsing.Whois.Eenet.Ee.Ee
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.eenet.ee/ee/Found", response.TemplateName);
+
+            Assert.AreEqual("google.ee", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2010, 05, 28, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2003, 04, 22, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("ADVOKAADIBÜROO SORAINEN AS", response.Registrant.Name);
+            Assert.AreEqual("5274536", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+372 6400 901", response.Registrant.FaxNumber);
+
+             // Registrant Address
+            Assert.AreEqual(1, response.Registrant.Address.Count);
+            Assert.AreEqual("PÄRNU MNT, 15, HARJUMAA TALLINN KESKLINN 10141", response.Registrant.Address[0]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("Mart Meier", response.AdminContact.Name);
+            Assert.AreEqual("mart.meier@sorainen.ee", response.AdminContact.Email);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("Joshua Hopping", response.TechnicalContact.Name);
+            Assert.AreEqual("ccops@markmonitor.com", response.TechnicalContact.Email);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+
+            Assert.AreEqual(14, response.FieldsParsed);
         }
     }
 }
