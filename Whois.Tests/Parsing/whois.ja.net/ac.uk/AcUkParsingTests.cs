4861c4ef5c67da7e249559a908e95281c243d49d
Chris Wood <chris@flipbit.co.uk>
2019-09-09 20:48:56 +0100

    Add parsing of .ac.uk, .gov.uk domains

--- Whois.Tests/Parsing/whois.ja.net/ac.uk/AcUkParsingTests.cs
+++ Whois.Tests/Parsing/whois.ja.net/ac.uk/AcUkParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Ja.Net.AcUk
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class AcUkParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,14 @@ namespace Whois.Parsing.Whois.Ja.Net.AcUk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.ja.net/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.ac.uk", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +44,44 @@ namespace Whois.Parsing.Whois.Ja.Net.AcUk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.ja.net/Found", response.TemplateName);
+
+            Assert.AreEqual("lboro.ac.uk", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("Jisc Collections and Janet Limited", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2013, 03, 20, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2003, 11, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 06, 16, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("Loughborough University", response.Registrant.Name);
+
+
+             // AdminContact Details
+            Assert.AreEqual("M S Cook", response.AdminContact.Name);
+            Assert.AreEqual("+44 1509 223498", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("+44 1509 223989", response.AdminContact.FaxNumber);
+            Assert.AreEqual("m.s.cook@lboro.ac.uk", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(3, response.AdminContact.Address.Count);
+            Assert.AreEqual("Computing Services, Loughborough University, Loughborough, Leicestershire", response.AdminContact.Address[0]);
+            Assert.AreEqual("LE11 3TU", response.AdminContact.Address[1]);
+            Assert.AreEqual("United Kingdom", response.AdminContact.Address[2]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("agate.lut.ac.uk", response.NameServers[0]);
+            Assert.AreEqual("bgate.lut.ac.uk", response.NameServers[1]);
+            Assert.AreEqual("cgate.lut.ac.uk", response.NameServers[2]);
+            Assert.AreEqual("ns3.ja.net", response.NameServers[3]);
+
+            Assert.AreEqual(18, response.FieldsParsed);
         }
     }
 }
--- Whois.Tests/Parsing/whois.ja.net/gov.uk/GovUkParsingTests.cs
+++ Whois.Tests/Parsing/whois.ja.net/gov.uk/GovUkParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Ja.Net.GovUk
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class GovUkParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Ja.Net.GovUk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.ja.net/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.gov.uk", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,50 @@ namespace Whois.Parsing.Whois.Ja.Net.GovUk
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.ja.net/Found", response.TemplateName);
+
+            Assert.AreEqual("direct.gov.uk", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("NTT Europe Online Ltd", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2010, 01, 13, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2003, 09, 17, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2011, 03, 30, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("Directgov", response.Registrant.Name);
+
+
+             // AdminContact Details
+            Assert.AreEqual("Directgov Director", response.AdminContact.Name);
+            Assert.AreEqual("+44 207 261 8723", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("+44 207 261 8696", response.AdminContact.FaxNumber);
+            Assert.AreEqual("helpdesk@directgov.gsi.gov.uk", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(5, response.AdminContact.Address.Count);
+            Assert.AreEqual("Hercules House", response.AdminContact.Address[0]);
+            Assert.AreEqual("Hercules Road", response.AdminContact.Address[1]);
+            Assert.AreEqual("London", response.AdminContact.Address[2]);
+            Assert.AreEqual("SE1 7DU", response.AdminContact.Address[3]);
+            Assert.AreEqual("United Kingdom", response.AdminContact.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(8, response.NameServers.Count);
+            Assert.AreEqual("eur5.akam.net", response.NameServers[0]);
+            Assert.AreEqual("eur6.akam.net", response.NameServers[1]);
+            Assert.AreEqual("ns1-173.akam.net", response.NameServers[2]);
+            Assert.AreEqual("ns1-31.akam.net", response.NameServers[3]);
+            Assert.AreEqual("usc4.akam.net", response.NameServers[4]);
+            Assert.AreEqual("use10.akam.net", response.NameServers[5]);
+            Assert.AreEqual("usw2.akam.net", response.NameServers[6]);
+            Assert.AreEqual("usw4.akam.net", response.NameServers[7]);
+
+            Assert.AreEqual(24, response.FieldsParsed);
         }
     }
 }
