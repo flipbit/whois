d8dfdcc3813fe1ff63ebec58d2ec335854133c6f
Chris Wood <chris@flipbit.co.uk>
2019-09-12 04:50:33 +0100

    Add parsing of .org.eu domains

--- Whois.Tests/Parsing/whois.eu.org/eu.org/EuOrgParsingTests.cs
+++ Whois.Tests/Parsing/whois.eu.org/eu.org/EuOrgParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Eu.Org.EuOrg
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class EuOrgParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Eu.Org.EuOrg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.eu.org/eu.org/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,49 @@ namespace Whois.Parsing.Whois.Eu.Org.EuOrg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/Found03", response.TemplateName);
+
+            Assert.AreEqual("google.eu.org", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2003, 03, 27, 23, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+
+             // Registrant Details
+            Assert.AreEqual("Mueller Michael", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("Wilhelm-Busch-Str. 35", response.Registrant.Address[0]);
+            Assert.AreEqual("32108 Bad Salzuflen", response.Registrant.Address[1]);
+            Assert.AreEqual("Germany", response.Registrant.Address[2]);
+
+             // AdminContact Details
+            Assert.AreEqual("MM114-FREE", response.AdminContact.RegistryId);
+            Assert.AreEqual("Mueller Michael", response.AdminContact.Name);
+            Assert.AreEqual("+49 005222 94569", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("mm114-d7ea0ef920c90b777acb325103212a7c@handles.eu.org", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(3, response.AdminContact.Address.Count);
+            Assert.AreEqual("Wilhelm-Busch-Str. 35", response.AdminContact.Address[0]);
+            Assert.AreEqual("32108 Bad Salzuflen", response.AdminContact.Address[1]);
+            Assert.AreEqual("Germany", response.AdminContact.Address[2]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("MM114-FREE", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Mueller Michael", response.TechnicalContact.Name);
+            Assert.AreEqual("+49 005222 94569", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("mm114-d7ea0ef920c90b777acb325103212a7c@handles.eu.org", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Wilhelm-Busch-Str. 35", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("32108 Bad Salzuflen", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("Germany", response.TechnicalContact.Address[2]);
+
+            Assert.AreEqual(16, response.FieldsParsed);
         }
     }
 }
