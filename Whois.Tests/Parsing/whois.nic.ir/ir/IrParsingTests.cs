652404c2aff52c2bff4d386484018531f0e2af22
Chris Wood <chris@flipbit.co.uk>
2019-09-14 11:30:39 +0100

    Add parsing of .ir domains

--- Whois.Tests/Parsing/whois.nic.ir/ir/IrParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.ir/ir/IrParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Ir.Ir
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class IrParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Nic.Ir.Ir
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.ir/ir/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.ir", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,59 @@ namespace Whois.Parsing.Whois.Nic.Ir.Ir
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.ir/ir/Found", response.TemplateName);
+
+            Assert.AreEqual("google.ir", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2014, 02, 17, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2014, 12, 22, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("go438-irnic", response.Registrant.RegistryId);
+            Assert.AreEqual("Google Inc.", response.Registrant.Organization);
+            Assert.AreEqual("+1 650 623 4000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1 650 618 8571", response.Registrant.FaxNumber);
+            Assert.AreEqual("support@domainservicesltd.co.uk", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(1, response.Registrant.Address.Count);
+            Assert.AreEqual("1600 Amphitheatre Parkway, Mountain View, CA, US", response.Registrant.Address[0]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("do210-irnic", response.AdminContact.RegistryId);
+            Assert.AreEqual("Domain Services Ltd", response.AdminContact.Organization);
+            Assert.AreEqual("+44 87 20229870", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("+44 87 20229871", response.AdminContact.FaxNumber);
+            Assert.AreEqual("admin@domainservicesltd.co.uk", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(1, response.AdminContact.Address.Count);
+            Assert.AreEqual("2nd Floor 145-147 St.John Street, London, London, UK", response.AdminContact.Address[0]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("do210-irnic", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Domain Services Ltd", response.TechnicalContact.Organization);
+            Assert.AreEqual("+44 87 20229870", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("+44 87 20229871", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("admin@domainservicesltd.co.uk", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(1, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("2nd Floor 145-147 St.John Street, London, London, UK", response.TechnicalContact.Address[0]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns3.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns1.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(23, response.FieldsParsed);
         }
     }
 }
