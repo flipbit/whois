0b8180d5e3b3d2fb7cca1f535144709635d093e4
Chris Wood <chris@flipbit.co.uk>
2019-09-17 04:55:31 +0100

    Add parsing of .as domains

--- Whois.Tests/Parsing/whois.nic.as/as/AsParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.as/as/AsParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.As.As
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class AsParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Nic.As.As
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound001", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,32 @@ namespace Whois.Parsing.Whois.Nic.As.As
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.as/as/Found", response.TemplateName);
+
+            Assert.AreEqual("google.as", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("MarkMonitor Inc. (http://www.markmonitor.com)", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2000, 08, 02, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("Google, Inc.", response.Registrant.Name);
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ST_CL_UPDATEPROHIBITED ST_CL_DELETEPROHIBITED ST_CL_TRANSFERPROHIBITED", response.DomainStatus[0]);
+
+            Assert.AreEqual(10, response.FieldsParsed);
         }
     }
 }
