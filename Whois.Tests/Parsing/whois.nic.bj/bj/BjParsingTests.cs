34d341be08c878ce5b71bd55ae0c1e7f31f0a657
Chris Wood <chris@flipbit.co.uk>
2019-09-18 08:43:03 +0100

    Add parsing of .bj domains

--- Whois.Tests/Parsing/whois.nic.bj/bj/BjParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.bj/bj/BjParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Bj.Bj
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class BjParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Nic.Bj.Bj
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.bj/bj/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.bj", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,33 @@ namespace Whois.Parsing.Whois.Nic.Bj.Bj
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.bj/bj/Found", response.TemplateName);
+
+            Assert.AreEqual("google.bj", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2012, 08, 10, 08, 57, 22, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2009, 03, 25, 08, 57, 22, 000, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("GOOGLE INC (ED0155)", response.Registrant.Name);
+            Assert.AreEqual("+1.6506234000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1.6506188571", response.Registrant.FaxNumber);
+
+             // Registrant Address
+            Assert.AreEqual(2, response.Registrant.Address.Count);
+            Assert.AreEqual("USA", response.Registrant.Address[0]);
+            Assert.AreEqual("1600 Amphitheatre Parkway, Moutain View CA 94043, US", response.Registrant.Address[1]);
+
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+
+            Assert.AreEqual(12, response.FieldsParsed);
         }
     }
 }
