0dc43fe57efe21cc9e7752e7c356243b5e9d22d6
Chris Wood <chris@flipbit.co.uk>
2019-09-21 10:29:37 +0100

    Add parsing of .es domains

--- Whois.Tests/Parsing/whois.nic.es/es/EsParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.es/es/EsParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Es.Es
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class EsParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Nic.Es.Es
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.es/es/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,26 @@ namespace Whois.Parsing.Whois.Nic.Es.Es
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.es/es/Found", response.TemplateName);
+
+            Assert.AreEqual("google.es", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2013, 10, 10, 07, 00, 02, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2003, 09, 16, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2014, 09, 16, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("GOOGLE INC.", response.Registrant.Name);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns2.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns1.google.com", response.NameServers[1]);
+
+            Assert.AreEqual(8, response.FieldsParsed);
         }
     }
 }
