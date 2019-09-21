efdd90d778831ced0ab04c8276cdffddac5bafd5
Chris Wood <chris@flipbit.co.uk>
2019-09-21 05:53:21 +0100

    Add parsing of .xn--p1ai domains

--- Whois.Tests/Parsing/whois.tcinet.ru/xn--p1ai/XnP1aiParsingTests.cs
+++ Whois.Tests/Parsing/whois.tcinet.ru/xn--p1ai/XnP1aiParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Tcinet.Ru.XnP1ai
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class XnP1aiParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Tcinet.Ru.XnP1ai
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.tcinet.ru/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,34 @@ namespace Whois.Parsing.Whois.Tcinet.Ru.XnP1ai
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.tcinet.ru/Found", response.TemplateName);
+
+            Assert.AreEqual("xn----8sbc3ahklcs4adf.xn--p1ai", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("R01-REG-RF", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2010, 11, 12, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2011, 11, 12, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("+7 800 3020800", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("liderkubani@gmail.com", response.Registrant.Email);
+
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.redsoft.ru.", response.NameServers[0]);
+            Assert.AreEqual("ns2.redsoft.ru.", response.NameServers[1]);
+            Assert.AreEqual("ns2.r01.ru.", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("REGISTERED, DELEGATED, VERIFIED", response.DomainStatus[0]);
+
+            Assert.AreEqual(11, response.FieldsParsed);
         }
     }
 }
