080fced9a39fbac04657cebabe5f431f1dab4462
Chris Wood <chris@flipbit.co.uk>
2019-09-22 04:36:46 +0100

    Add parsing of .fo domains

--- Whois.Tests/Parsing/whois.nic.fo/fo/FoParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.fo/fo/FoParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Fo.Fo
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class FoParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,12 @@ namespace Whois.Parsing.Whois.Nic.Fo.Fo
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.fo/fo/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +42,55 @@ namespace Whois.Parsing.Whois.Nic.Fo.Fo
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.fo/fo/Found", response.TemplateName);
+
+            Assert.AreEqual("nic.fo", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2011, 07, 12, 12, 52, 57, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2010, 06, 03, 03, 34, 05, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2015, 01, 03, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("ID005359", response.Registrant.RegistryId);
+            Assert.AreEqual("FO-umsitingin", response.Registrant.Name);
+            Assert.AreEqual(new DateTime(2010, 07, 21, 19, 11, 55, 000, DateTimeKind.Utc), response.Registrant.Created);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("Hoydalsvegur 19, Postboks 1255", response.Registrant.Address[0]);
+            Assert.AreEqual("Torshavn", response.Registrant.Address[1]);
+            Assert.AreEqual("110", response.Registrant.Address[2]);
+            Assert.AreEqual("FO", response.Registrant.Address[3]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("ID005359", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("FO-umsitingin", response.TechnicalContact.Name);
+            Assert.AreEqual(new DateTime(2010, 07, 21, 19, 11, 55, 000, DateTimeKind.Utc), response.TechnicalContact.Created);
+
+             // TechnicalContact Address
+            Assert.AreEqual(4, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Hoydalsvegur 19, Postboks 1255", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Torshavn", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("110", response.TechnicalContact.Address[2]);
+            Assert.AreEqual("FO", response.TechnicalContact.Address[3]);
+
+
+            // Nameservers
+            Assert.AreEqual(5, response.NameServers.Count);
+            Assert.AreEqual("ns1.gratisdns.dk", response.NameServers[0]);
+            Assert.AreEqual("ns2.gratisdns.dk", response.NameServers[1]);
+            Assert.AreEqual("ns3.gratisdns.dk", response.NameServers[2]);
+            Assert.AreEqual("ns4.gratisdns.dk", response.NameServers[3]);
+            Assert.AreEqual("ns5.gratisdns.dk", response.NameServers[4]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("paid and in zone", response.DomainStatus[0]);
+
+            Assert.AreEqual(21, response.FieldsParsed);
         }
     }
 }
