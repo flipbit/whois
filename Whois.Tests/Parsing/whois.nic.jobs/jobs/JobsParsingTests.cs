4aedca796440284a7a81fd64ca400ab56c49a0cd
Chris Wood <chris@flipbit.co.uk>
2019-09-22 05:40:37 +0100

    Add parsing of .jobs domains

--- Whois.Tests/Parsing/whois.nic.jobs/jobs/JobsParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.jobs/jobs/JobsParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Jobs.Jobs
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class JobsParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,19 @@ namespace Whois.Parsing.Whois.Nic.Jobs.Jobs
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.jobs/jobs/Found", response.TemplateName);
+
+            Assert.AreEqual("example.jobs", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("EMPLOY MEDIA LLC", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2006, 02, 23, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2006, 02, 23, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+
+            Assert.AreEqual(5, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +49,13 @@ namespace Whois.Parsing.Whois.Nic.Jobs.Jobs
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.jobs/jobs/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.jobs", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +66,38 @@ namespace Whois.Parsing.Whois.Nic.Jobs.Jobs
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.jobs/jobs/Found", response.TemplateName);
+
+            Assert.AreEqual("google.jobs", response.DomainName);
+            Assert.AreEqual("86932313_DOMAIN_JOBS-VRSN", response.RegistryDomainId);
+
+            // Registrar Details
+            Assert.AreEqual("MARKMONITOR INC.", response.Registrar.Name);
+            Assert.AreEqual("292", response.Registrar.IanaId);
+            Assert.AreEqual("http://www.markmonitor.com", response.Registrar.Url);
+            Assert.AreEqual("whois.markmonitor.com", response.Registrar.WhoisServerUrl);
+            Assert.AreEqual("abusecomplaints@markmonitor.com", response.Registrar.AbuseEmail);
+            Assert.AreEqual("+1.2083895740", response.Registrar.AbuseTelephoneNumber);
+
+            Assert.AreEqual(new DateTime(2017, 07, 27, 20, 59, 01, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2005, 09, 15, 04, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2017, 09, 15, 04, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(3, response.DomainStatus.Count);
+            Assert.AreEqual("clientDeleteProhibited", response.DomainStatus[0]);
+            Assert.AreEqual("clientTransferProhibited", response.DomainStatus[1]);
+            Assert.AreEqual("clientUpdateProhibited", response.DomainStatus[2]);
+
+            Assert.AreEqual("unsigned", response.DnsSecStatus);
+            Assert.AreEqual(18, response.FieldsParsed);
         }
     }
 }
