c3ddfee2de8f902691c317c5890b7bd18149ba9e
Chris Wood <chris@flipbit.co.uk>
2019-09-24 05:09:39 +0100

    Add parsing of .sh domains

--- Whois.Tests/Parsing/whois.nic.sh/sh/ShParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.sh/sh/ShParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Sh.Sh
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class ShParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,8 @@ namespace Whois.Parsing.Whois.Nic.Sh.Sh
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
         }
 
         [Test]
@@ -36,6 +38,36 @@ namespace Whois.Parsing.Whois.Nic.Sh.Sh
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.sh/sh/Found", response.TemplateName);
+
+            Assert.AreEqual("google.sh", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2014, 06, 06, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("DNS Admin", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(5, response.Registrant.Address.Count);
+            Assert.AreEqual("Google Inc.", response.Registrant.Address[0]);
+            Assert.AreEqual("1600 Amphitheatre Parkway", response.Registrant.Address[1]);
+            Assert.AreEqual("Mountain View", response.Registrant.Address[2]);
+            Assert.AreEqual("CA", response.Registrant.Address[3]);
+            Assert.AreEqual("US", response.Registrant.Address[4]);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Live", response.DomainStatus[0]);
+
+            Assert.AreEqual(12, response.FieldsParsed);
         }
     }
 }
