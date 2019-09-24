2a41fa0728f4c77634fa191daea432c729847735
Chris Wood <chris@flipbit.co.uk>
2019-09-24 05:53:31 +0100

    Add parsing of .sn domains

--- Whois.Tests/Parsing/whois.nic.sn/sn/SnParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.sn/sn/SnParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Sn.Sn
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class SnParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,8 @@ namespace Whois.Parsing.Whois.Nic.Sn.Sn
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
         }
 
         [Test]
@@ -36,6 +38,37 @@ namespace Whois.Parsing.Whois.Nic.Sn.Sn
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.sn/sn/Found", response.TemplateName);
+
+            Assert.AreEqual("google.sn", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("registry", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2008, 05, 08, 17, 59, 38, 430, DateTimeKind.Utc), response.Registered);
+
+             // Registrant Details
+            Assert.AreEqual("C4-SN", response.Registrant.RegistryId);
+
+
+             // AdminContact Details
+            Assert.AreEqual("C5-SN", response.AdminContact.RegistryId);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("C6-SN", response.TechnicalContact.RegistryId);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            Assert.AreEqual(11, response.FieldsParsed);
         }
     }
 }
