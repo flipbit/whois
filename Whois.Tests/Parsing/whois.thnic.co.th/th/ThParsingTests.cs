cc2401d11fb197711efee472a5e7215790fd4690
Chris Wood <chris@flipbit.co.uk>
2019-09-25 05:51:45 +0100

    Add parsing of .th domains

--- Whois.Tests/Parsing/whois.thnic.co.th/th/ThParsingTests.cs
+++ Whois.Tests/Parsing/whois.thnic.co.th/th/ThParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Thnic.Co.Th.Th
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class ThParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,13 @@ namespace Whois.Parsing.Whois.Thnic.Co.Th.Th
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.thnic.co.th/th/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.co.th", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +43,53 @@ namespace Whois.Parsing.Whois.Thnic.Co.Th.Th
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.thnic.co.th/th/Found", response.TemplateName);
+
+            Assert.AreEqual("google.co.th", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("T.H.NIC Co., Ltd.", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2012, 09, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+            Assert.AreEqual(new DateTime(2004, 10, 08, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2013, 10, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("Google Inc.", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("2400 Bayshore Parkway, Mountain Veiw, CA", response.Registrant.Address[0]);
+            Assert.AreEqual("94043", response.Registrant.Address[1]);
+            Assert.AreEqual("US", response.Registrant.Address[2]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("13244", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("MarkMonitor Inc.", response.TechnicalContact.Name);
+
+             // TechnicalContact Address
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("391 N Ancestor Place, Boise, ID", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("83704", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("US", response.TechnicalContact.Address[2]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ACTIVE", response.DomainStatus[0]);
+
+            Assert.AreEqual(20, response.FieldsParsed);
         }
     }
 }
