6e55da4aeca348f17f69f594d7510af8d75a9db4
Chris Wood <chris@flipbit.co.uk>
2019-09-25 05:02:54 +0100

    Add parsing of .qa domains

--- Whois.Tests/Parsing/whois.registry.qa/qa/QaParsingTests.cs
+++ Whois.Tests/Parsing/whois.registry.qa/qa/QaParsingTests.cs
@@ -5,7 +5,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Registry.Qa.Qa
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class QaParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +25,37 @@ namespace Whois.Parsing.Whois.Registry.Qa.Qa
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.qa/qa/Found", response.TemplateName);
+
+            Assert.AreEqual("qnb.com.qa", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("Qatar Telecom (Qtel) Q. S. C", response.Registrar.Name);
+
+             // Registrant Details
+            Assert.AreEqual("QT40975", response.Registrant.RegistryId);
+            Assert.AreEqual("DNS Administrator - Qtel Internet Services", response.Registrant.Name);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("QT40975", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("DNS Administrator - Qtel Internet Services", response.TechnicalContact.Name);
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns1.qatarbank.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.qatarbank.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.qatarbank.com", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(3, response.DomainStatus.Count);
+            Assert.AreEqual("serverDeleteProhibited (Legacy)", response.DomainStatus[0]);
+            Assert.AreEqual("serverRenewProhibited (Legacy)", response.DomainStatus[1]);
+            Assert.AreEqual("serverTransferProhibited (Legacy)", response.DomainStatus[2]);
+
+            Assert.AreEqual(13, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +66,11 @@ namespace Whois.Parsing.Whois.Registry.Qa.Qa
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.qa/qa/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +81,36 @@ namespace Whois.Parsing.Whois.Registry.Qa.Qa
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.qa/qa/Found", response.TemplateName);
+
+            Assert.AreEqual("qtel.com.qa", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("Qatar Telecom (Qtel) Q. S. C", response.Registrar.Name);
+
+
+             // Registrant Details
+            Assert.AreEqual("QT11734", response.Registrant.RegistryId);
+            Assert.AreEqual("DNS Administrator - Qtel Internet Services", response.Registrant.Name);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("QT11734", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("DNS Administrator - Qtel Internet Services", response.TechnicalContact.Name);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.qtel.com.qa", response.NameServers[0]);
+            Assert.AreEqual("ns2.qtel.com.qa", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual(10, response.FieldsParsed);
         }
     }
 }
