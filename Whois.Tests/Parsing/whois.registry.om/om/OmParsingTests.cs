78900002caa09a13b90d5e32610e9eab6f16e747
Chris Wood <chris@flipbit.co.uk>
2019-09-25 04:56:08 +0100

    Add parsing of .om domains

--- Whois.Tests/Parsing/whois.registry.om/om/OmParsingTests.cs
+++ Whois.Tests/Parsing/whois.registry.om/om/OmParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Registry.Om.Om
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class OmParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Registry.Om.Om
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.om/om/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,48 @@ namespace Whois.Parsing.Whois.Registry.Om.Om
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.om/om/Found", response.TemplateName);
+
+            Assert.AreEqual("rop.gov.om", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("Oman Telecommunication Company", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2013, 10, 06, 18, 20, 12, 000, DateTimeKind.Utc), response.Updated);
+
+             // Registrant Details
+            Assert.AreEqual("10084244", response.Registrant.RegistryId);
+            Assert.AreEqual("Nasser Said Al Daree", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(2, response.Registrant.Address.Count);
+            Assert.AreEqual("Mina Al Fahal", response.Registrant.Address[0]);
+            Assert.AreEqual("om", response.Registrant.Address[1]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("10084244", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("Nasser Said Al Daree", response.TechnicalContact.Name);
+
+             // TechnicalContact Address
+            Assert.AreEqual(2, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Mina Al Fahal", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("om", response.TechnicalContact.Address[1]);
+
+
+            // Nameservers
+            Assert.AreEqual(3, response.NameServers.Count);
+            Assert.AreEqual("ns3.omantel.net.om", response.NameServers[0]);
+            Assert.AreEqual("ns1.omantel.net.om", response.NameServers[1]);
+            Assert.AreEqual("ns2.omantel.net.om", response.NameServers[2]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("ok", response.DomainStatus[0]);
+
+            Assert.AreEqual(16, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +93,11 @@ namespace Whois.Parsing.Whois.Registry.Om.Om
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Reserved, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.om/om/Reserved", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
     }
 }
