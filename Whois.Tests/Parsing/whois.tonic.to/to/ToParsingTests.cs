021695a6cdf9bbac5fc433187c1502b75918fb52
Chris Wood <chris@flipbit.co.uk>
2019-09-25 08:59:43 +0100

    Add parsing of .to domains

--- Whois.Tests/Parsing/whois.tonic.to/to/ToParsingTests.cs
+++ Whois.Tests/Parsing/whois.tonic.to/to/ToParsingTests.cs
@@ -5,7 +5,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Tonic.To.To
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class ToParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +25,13 @@ namespace Whois.Parsing.Whois.Tonic.To.To
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.tonic.to/to/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +42,16 @@ namespace Whois.Parsing.Whois.Tonic.To.To
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.tonic.to/to/Found", response.TemplateName);
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns-1.myphotoalbum.com", response.NameServers[0]);
+            Assert.AreEqual("ns-2.myphotoalbum.com", response.NameServers[1]);
+
+            Assert.AreEqual(3, response.FieldsParsed);
         }
     }
 }
