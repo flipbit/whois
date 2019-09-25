96f2a9a4af66c03deab8c15f8eb8276b15113e22
Chris Wood <chris@flipbit.co.uk>
2019-09-25 09:07:37 +0100

    Add parsing of .fj domains

--- Whois.Tests/Parsing/whois.usp.ac.fj/fj/FjParsingTests.cs
+++ Whois.Tests/Parsing/whois.usp.ac.fj/fj/FjParsingTests.cs
@@ -5,7 +5,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Usp.Ac.Fj.Fj
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class FjParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +25,13 @@ namespace Whois.Parsing.Whois.Usp.Ac.Fj.Fj
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.usp.ac.fj/fj/NotFound", response.TemplateName);
+
+            Assert.AreEqual("u34jedzcq.fj", response.DomainName);
+
+            Assert.AreEqual(2, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +42,29 @@ namespace Whois.Parsing.Whois.Usp.Ac.Fj.Fj
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.usp.ac.fj/fj/Found", response.TemplateName);
+
+            Assert.AreEqual("google.com.fj", response.DomainName);
+
+             // Registrant Details
+            Assert.AreEqual("Google Inc.", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(1, response.Registrant.Address.Count);
+            Assert.AreEqual("2400 E. Bayshore Pkwy", response.Registrant.Address[0]);
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Active", response.DomainStatus[0]);
+
+            Assert.AreEqual(7, response.FieldsParsed);
         }
     }
 }
