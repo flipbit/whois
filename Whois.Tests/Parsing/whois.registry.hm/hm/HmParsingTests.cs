2ffbac703ecdb93c7f9bc948575c8254bd547154
Chris Wood <chris@flipbit.co.uk>
2019-09-26 14:24:55 +0100

    Add parsing of .hm domains

--- Whois.Tests/Parsing/whois.registry.hm/hm/HmParsingTests.cs
+++ Whois.Tests/Parsing/whois.registry.hm/hm/HmParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Registry.Hm.Hm
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class HmParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,12 @@ namespace Whois.Parsing.Whois.Registry.Hm.Hm
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound003", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +42,78 @@ namespace Whois.Parsing.Whois.Registry.Hm.Hm
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.registry.hm/hm/Found", response.TemplateName);
+
+            Assert.AreEqual("google.hm", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("HM Domain Registry", response.Registrar.Name);
+            Assert.AreEqual("http://www.registry.hm/", response.Registrar.Url);
+
+            Assert.AreEqual(new DateTime(2003, 04, 28, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2020, 04, 28, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("John G Rose", response.Registrant.Name);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("1058 Jasmine St.", response.Registrant.Address[0]);
+            Assert.AreEqual("Denver CO 80220", response.Registrant.Address[1]);
+            Assert.AreEqual("US", response.Registrant.Address[2]);
+
+
+             // AdminContact Details
+            Assert.AreEqual("ROS00074", response.AdminContact.RegistryId);
+            Assert.AreEqual("John Rose", response.AdminContact.Name);
+            Assert.AreEqual("Sigma Polyplexic", response.AdminContact.Organization);
+            Assert.AreEqual("2129996566", response.AdminContact.TelephoneNumber);
+            Assert.AreEqual("johnrose@polyplexic.com", response.AdminContact.Email);
+
+             // AdminContact Address
+            Assert.AreEqual(3, response.AdminContact.Address.Count);
+            Assert.AreEqual("1058 Jasmine St.", response.AdminContact.Address[0]);
+            Assert.AreEqual("Denver CO 80220", response.AdminContact.Address[1]);
+            Assert.AreEqual("US", response.AdminContact.Address[2]);
+
+
+             // BillingContact Details
+            Assert.AreEqual("ROS00074", response.BillingContact.RegistryId);
+            Assert.AreEqual("John Rose", response.BillingContact.Name);
+            Assert.AreEqual("Sigma Polyplexic", response.BillingContact.Organization);
+            Assert.AreEqual("2129996566", response.BillingContact.TelephoneNumber);
+            Assert.AreEqual("johnrose@polyplexic.com", response.BillingContact.Email);
+
+             // BillingContact Address
+            Assert.AreEqual(3, response.BillingContact.Address.Count);
+            Assert.AreEqual("1058 Jasmine St.", response.BillingContact.Address[0]);
+            Assert.AreEqual("Denver CO 80220", response.BillingContact.Address[1]);
+            Assert.AreEqual("US", response.BillingContact.Address[2]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("ROS00074", response.TechnicalContact.RegistryId);
+            Assert.AreEqual("John Rose", response.TechnicalContact.Name);
+            Assert.AreEqual("Sigma Polyplexic", response.TechnicalContact.Organization);
+            Assert.AreEqual("2129996566", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("johnrose@polyplexic.com", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("1058 Jasmine St.", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("Denver CO 80220", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("US", response.TechnicalContact.Address[2]);
+
+
+            // Nameservers
+            Assert.AreEqual(2, response.NameServers.Count);
+            Assert.AreEqual("ns1.everydns.net", response.NameServers[0]);
+            Assert.AreEqual("ns2.everydns.net", response.NameServers[1]);
+
+            Assert.AreEqual(36, response.FieldsParsed);
         }
     }
 }
