b2e57526f46d041a83ee9cc47fbc47ffbdea7082
Chris Wood <chris@flipbit.co.uk>
2019-09-26 12:48:28 +0100

    Add parsing of .sm domains

--- Whois.Tests/Parsing/whois.nic.sm/sm/SmParsingTests.cs
+++ Whois.Tests/Parsing/whois.nic.sm/sm/SmParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Nic.Sm.Sm
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class SmParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,11 @@ namespace Whois.Parsing.Whois.Nic.Sm.Sm
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.sm/sm/NotFound", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +41,54 @@ namespace Whois.Parsing.Whois.Nic.Sm.Sm
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.nic.sm/sm/Found", response.TemplateName);
+
+            Assert.AreEqual("google.sm", response.DomainName);
+
+            Assert.AreEqual(new DateTime(2008, 08, 01, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+
+             // Registrant Details
+            Assert.AreEqual("Rose Hagan", response.Registrant.Name);
+            Assert.AreEqual("Google, Inc.", response.Registrant.Organization);
+            Assert.AreEqual("+1 650 2530000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1 650 6188571", response.Registrant.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("1600 Amphitheatre Parkway", response.Registrant.Address[0]);
+            Assert.AreEqual("US 94043 Mountain View (CA)", response.Registrant.Address[1]);
+            Assert.AreEqual("US", response.Registrant.Address[2]);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("Domain Names Department", response.TechnicalContact.Name);
+            Assert.AreEqual("Visiant Outsourcing S.r.l.", response.TechnicalContact.Organization);
+            Assert.AreEqual("+39 011 3473520", response.TechnicalContact.TelephoneNumber);
+            Assert.AreEqual("+39 011 3473522", response.TechnicalContact.FaxNumber);
+            Assert.AreEqual("domains.outsourcing@visiant.it", response.TechnicalContact.Email);
+
+             // TechnicalContact Address
+            Assert.AreEqual(3, response.TechnicalContact.Address.Count);
+            Assert.AreEqual("Strada del Drosso 128/6", response.TechnicalContact.Address[0]);
+            Assert.AreEqual("I 10135 Torino (TO)", response.TechnicalContact.Address[1]);
+            Assert.AreEqual("IT", response.TechnicalContact.Address[2]);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("Active", response.DomainStatus[0]);
+
+            Assert.AreEqual(24, response.FieldsParsed);
         }
     }
 }
