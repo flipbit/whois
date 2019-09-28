7a1df4973c8356dd8959740ad75e01ccb5ebb51f
Chris Wood <chris@flipbit.co.uk>
2019-09-28 04:09:51 +0100

    Add parsing of .sg domains

--- Whois.Tests/Parsing/whois.sgnic.sg/sg/SgParsingTests.cs
+++ Whois.Tests/Parsing/whois.sgnic.sg/sg/SgParsingTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Whois.Models;
 using Whois.Parsers;
@@ -5,7 +6,6 @@ using Whois.Parsers;
 namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 {
     [TestFixture]
-    [Ignore("TODO")]
     public class SgParsingTests : ParsingTests
     {
         private WhoisParser parser;
@@ -26,6 +26,40 @@ namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sgnic.sg/sg/Found01", response.TemplateName);
+
+            Assert.AreEqual("google.sg", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("MARKMONITOR INC", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2005, 01, 03, 12, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2011, 01, 03, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("GOOGLE INC.", response.Registrant.Name);
+            Assert.AreEqual("+1.6503300100", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1.6506181434", response.Registrant.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("1600 AMPHITHEATRE PARKWAY", response.Registrant.Address[0]);
+            Assert.AreEqual("CA", response.Registrant.Address[1]);
+            Assert.AreEqual("US", response.Registrant.Address[2]);
+            Assert.AreEqual("94043", response.Registrant.Address[3]);
+
+
+            // Domain Status
+            Assert.AreEqual(4, response.DomainStatus.Count);
+            Assert.AreEqual("OK", response.DomainStatus[0]);
+            Assert.AreEqual("CLIENT UPDATE PROHIBITED", response.DomainStatus[1]);
+            Assert.AreEqual("CLIENT TRANSFER PROHIBITED", response.DomainStatus[2]);
+            Assert.AreEqual("CLIENT DELETE PROHIBITED", response.DomainStatus[3]);
+
+            Assert.AreEqual(18, response.FieldsParsed);
         }
 
         [Test]
@@ -36,6 +70,37 @@ namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            AssertWriter.Write(response);
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sgnic.sg/sg/Found01", response.TemplateName);
+
+            Assert.AreEqual("canon.com.sg", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("SINGNET PTE LTD", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(1996, 01, 09, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2012, 01, 09, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("CANON SINGAPORE PTE. LTD.", response.Registrant.Name);
+            Assert.AreEqual("67845922", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("64753273", response.Registrant.FaxNumber);
+            Assert.AreEqual("hostmaster@singnet.com.sg", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(3, response.Registrant.Address.Count);
+            Assert.AreEqual("1 HarbourFront Avenue", response.Registrant.Address[0]);
+            Assert.AreEqual("SG", response.Registrant.Address[1]);
+            Assert.AreEqual("098632", response.Registrant.Address[2]);
+
+
+            // Domain Status
+            Assert.AreEqual(1, response.DomainStatus.Count);
+            Assert.AreEqual("OK", response.DomainStatus[0]);
+
+            Assert.AreEqual(14, response.FieldsParsed);
         }
 
         [Test]
@@ -46,6 +111,40 @@ namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sgnic.sg/sg/Found01", response.TemplateName);
+
+            Assert.AreEqual("google.sg", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("MARKMONITOR INC", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2005, 01, 03, 12, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2012, 01, 03, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("GOOGLE INC.", response.Registrant.Name);
+            Assert.AreEqual("+1.6502530000", response.Registrant.TelephoneNumber);
+            Assert.AreEqual("+1.6502530001", response.Registrant.FaxNumber);
+            Assert.AreEqual("dns-admin@google.com", response.Registrant.Email);
+
+             // Registrant Address
+            Assert.AreEqual(4, response.Registrant.Address.Count);
+            Assert.AreEqual("1600 AMPHITHEATRE PARKWAY", response.Registrant.Address[0]);
+            Assert.AreEqual("CA", response.Registrant.Address[1]);
+            Assert.AreEqual("US", response.Registrant.Address[2]);
+            Assert.AreEqual("94043", response.Registrant.Address[3]);
+
+
+            // Domain Status
+            Assert.AreEqual(4, response.DomainStatus.Count);
+            Assert.AreEqual("OK", response.DomainStatus[0]);
+            Assert.AreEqual("CLIENT UPDATE PROHIBITED", response.DomainStatus[1]);
+            Assert.AreEqual("CLIENT TRANSFER PROHIBITED", response.DomainStatus[2]);
+            Assert.AreEqual("CLIENT DELETE PROHIBITED", response.DomainStatus[3]);
+
+            Assert.AreEqual(18, response.FieldsParsed);
         }
 
         [Test]
@@ -56,6 +155,11 @@ namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.NotFound, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("generic/tld/NotFound002", response.TemplateName);
+
+            Assert.AreEqual(1, response.FieldsParsed);
         }
 
         [Test]
@@ -66,6 +170,46 @@ namespace Whois.Parsing.Whois.Sgnic.Sg.Sg
 
             Assert.Greater(sample.Length, 0);
             Assert.AreEqual(WhoisResponseStatus.Found, response.Status);
+
+            Assert.AreEqual(0, response.ParsingErrors);
+            Assert.AreEqual("whois.sgnic.sg/sg/Found02", response.TemplateName);
+
+            Assert.AreEqual("google.sg", response.DomainName);
+
+            // Registrar Details
+            Assert.AreEqual("MARKMONITOR INC", response.Registrar.Name);
+
+            Assert.AreEqual(new DateTime(2005, 01, 03, 12, 00, 00, 000, DateTimeKind.Utc), response.Registered);
+            Assert.AreEqual(new DateTime(2020, 01, 03, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+
+             // Registrant Details
+            Assert.AreEqual("GOOGLE LLC", response.Registrant.Name);
+
+
+             // AdminContact Details
+            Assert.AreEqual("MARKMONITOR INC.", response.AdminContact.Name);
+
+
+             // TechnicalContact Details
+            Assert.AreEqual("GOOGLE LLC", response.TechnicalContact.Name);
+
+
+            // Nameservers
+            Assert.AreEqual(4, response.NameServers.Count);
+            Assert.AreEqual("ns1.google.com", response.NameServers[0]);
+            Assert.AreEqual("ns2.google.com", response.NameServers[1]);
+            Assert.AreEqual("ns3.google.com", response.NameServers[2]);
+            Assert.AreEqual("ns4.google.com", response.NameServers[3]);
+
+            // Domain Status
+            Assert.AreEqual(5, response.DomainStatus.Count);
+            Assert.AreEqual("OK", response.DomainStatus[0]);
+            Assert.AreEqual("CLIENT UPDATE PROHIBITED", response.DomainStatus[1]);
+            Assert.AreEqual("CLIENT TRANSFER PROHIBITED", response.DomainStatus[2]);
+            Assert.AreEqual("CLIENT DELETE PROHIBITED", response.DomainStatus[3]);
+            Assert.AreEqual("VerifiedID@SG-Not Required", response.DomainStatus[4]);
+
+            Assert.AreEqual(17, response.FieldsParsed);
         }
     }
 }
