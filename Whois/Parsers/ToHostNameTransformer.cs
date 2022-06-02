966cec0c0470d32d500a2791e780527986fc99b5
flipbit <chris@flipbit.co.uk>
2022-06-02 05:19:26 +0100

    Fix .mx date parsing

--- Whois/Parsers/ToHostNameTransformer.cs
+++ Whois/Parsers/ToHostNameTransformer.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 using Tokens.Transformers;
 
-namespace Whois.Parsers
+namespace Whois.Parsers;
+
+/// <summary>
+/// Converts a string into a <see cref="HostName"/>
+/// </summary>
+public class ToHostNameTransformer : ITokenTransformer
 {
-    /// <summary>
-    /// Converts a string into a <see cref="HostName"/.
-    /// </summary>
-    public class ToHostNameTransformer : ITokenTransformer
+    public bool CanTransform(object value, string[] args, out object transformed)
     {
-        public bool CanTransform(object value, string[] args, out object transformed)
+        if (value == null)
         {
-            if (value == null)
-            {
-                transformed = null;
-                return true;
-            }
-
-            var valueString = value.ToString();
-
-            if (string.IsNullOrEmpty(valueString))
-            {
-                transformed = null;
-                return true;
-            }
-
-            if (HostName.TryParse(valueString, out var hostName))
-            {
-                transformed = hostName;
-                return true;
-            }
-
             transformed = null;
-
-            return false;
+            return true;
         }
+
+        var valueString = value.ToString();
+
+        if (string.IsNullOrEmpty(valueString))
+        {
+            transformed = null;
+            return true;
+        }
+
+        if (HostName.TryParse(valueString, out var hostName))
+        {
+            transformed = hostName;
+            return true;
+        }
+
+        transformed = null;
+
+        return false;
     }
 }
