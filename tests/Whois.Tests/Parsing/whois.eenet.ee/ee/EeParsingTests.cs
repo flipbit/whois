172ec8901a4b2536760f89f11bf82fc009dc3ad9
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:23 +0200

    fix: ee parsing

--- tests/Whois.Tests/Parsing/whois.eenet.ee/ee/EeParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.eenet.ee/ee/EeParsingTests.cs
@@ -13,7 +13,7 @@ public class EeParsingTests : ParsingTests
         parser = new WhoisParser();
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_not_found()
     {
         var sample = SampleReader.Read("whois.eenet.ee", "ee", "not-found", "u34jedzcq.ee.txt");
@@ -23,11 +23,11 @@ public class EeParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.NotFound, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.eenet.ee/ee/not-found/01", response.TemplateName);
+        Assert.Equal("generic/tld/not-found/03", response.TemplateName);
 
-        Assert.Equal("u34jedzcq.ee", response.DomainName.ToString());
+        Assert.Null(response.DomainName);
 
-        Assert.Equal(2, response.FieldsParsed);
+        Assert.Equal(1, response.FieldsParsed);
     }
 
     [Fact(Skip = "Template update deferred - WHOIS response format changed")]
