1493a94c8fb09ec7b51bf06be03f954ecc1f1783
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:25 +0200

    fix: nic.space parsing

--- tests/Whois.Tests/Parsing/whois.nic.space/space/SpaceParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.nic.space/space/SpaceParsingTests.cs
@@ -28,7 +28,7 @@ public class SpaceParsingTests : ParsingTests
         Assert.Equal(1, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found()
     {
         var sample = SampleReader.Read("whois.nic.space", "space", "found", "nic.space.txt");
@@ -38,99 +38,55 @@ public class SpaceParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.Found, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("generic/tld/found/01", response.TemplateName);
+        Assert.Equal("generic/tld/found/02", response.TemplateName);
 
         Assert.Equal("nic.space", response.DomainName.ToString());
         Assert.Equal("D2361836-CNIC", response.RegistryDomainId);
 
         // Registrar Details
-        Assert.Equal("CentralNic Ltd", response.Registrar.Name);
+        Assert.Equal("Radix Technologies Inc. SEZC / CO Services Cayman Limited", response.Registrar.Name);
         Assert.Equal("9999", response.Registrar.IanaId);
-        Assert.Equal("http://www.centralnic.com/", response.Registrar.Url);
-        Assert.Equal("whois.centralnic.com", response.Registrar.WhoisServer.Value);
+        Assert.Equal("https://radix.website/", response.Registrar.Url);
+        Assert.Equal("whois.radix.host", response.Registrar.WhoisServer.Value);
 
-        Assert.Equal(new DateTime(2015, 04, 04, 00, 14, 21, 000, DateTimeKind.Utc), response.Updated);
+        Assert.Equal(new DateTime(2026, 05, 26, 00, 02, 58, 101, DateTimeKind.Utc), response.Updated);
         Assert.Equal(new DateTime(2014, 04, 10, 09, 14, 07, 000, DateTimeKind.Utc), response.Registered);
-        Assert.Equal(new DateTime(2016, 04, 10, 23, 59, 59, 000, DateTimeKind.Utc), response.Expiration);
+        Assert.Equal(new DateTime(2027, 04, 10, 23, 59, 59, 000, DateTimeKind.Utc), response.Expiration);
 
         // Registrant Details
-        Assert.Equal("C11480", response.Registrant.RegistryId);
-        Assert.Equal("Domain Administrator", response.Registrant.Name);
-        Assert.Equal("CentralNic Ltd", response.Registrant.Organization);
-        Assert.Equal("+44.2033880600", response.Registrant.TelephoneNumber);
-        Assert.Equal("+44.2033880601", response.Registrant.FaxNumber);
-        Assert.Equal("domains@centralnic.com", response.Registrant.Email);
+        Assert.Null(response.Registrant);
 
         // Registrant Address
-        Assert.Equal(4, response.Registrant.Address.Count);
-        Assert.Equal("35-39 Moorgate", response.Registrant.Address[0]);
-        Assert.Equal("London", response.Registrant.Address[1]);
-        Assert.Equal("EC2R 6AR", response.Registrant.Address[2]);
-        Assert.Equal("GB", response.Registrant.Address[3]);
 
 
         // AdminContact Details
-        Assert.Equal("C11480", response.AdminContact.RegistryId);
-        Assert.Equal("Domain Administrator", response.AdminContact.Name);
-        Assert.Equal("CentralNic Ltd", response.AdminContact.Organization);
-        Assert.Equal("+44.2033880600", response.AdminContact.TelephoneNumber);
-        Assert.Equal("+44.2033880601", response.AdminContact.FaxNumber);
-        Assert.Equal("domains@centralnic.com", response.AdminContact.Email);
+        Assert.Null(response.AdminContact);
 
         // AdminContact Address
-        Assert.Equal(4, response.AdminContact.Address.Count);
-        Assert.Equal("35-39 Moorgate", response.AdminContact.Address[0]);
-        Assert.Equal("London", response.AdminContact.Address[1]);
-        Assert.Equal("EC2R 6AR", response.AdminContact.Address[2]);
-        Assert.Equal("GB", response.AdminContact.Address[3]);
 
 
         // BillingContact Details
-        Assert.Equal("C11480", response.BillingContact.RegistryId);
-        Assert.Equal("Domain Administrator", response.BillingContact.Name);
-        Assert.Equal("CentralNic Ltd", response.BillingContact.Organization);
-        Assert.Equal("+44.2033880600", response.BillingContact.TelephoneNumber);
-        Assert.Equal("+44.2033880601", response.BillingContact.FaxNumber);
-        Assert.Equal("domains@centralnic.com", response.BillingContact.Email);
+        Assert.Null(response.BillingContact);
 
         // BillingContact Address
-        Assert.Equal(4, response.BillingContact.Address.Count);
-        Assert.Equal("35-39 Moorgate", response.BillingContact.Address[0]);
-        Assert.Equal("London", response.BillingContact.Address[1]);
-        Assert.Equal("EC2R 6AR", response.BillingContact.Address[2]);
-        Assert.Equal("GB", response.BillingContact.Address[3]);
 
 
         // TechnicalContact Details
-        Assert.Equal("C11480", response.TechnicalContact.RegistryId);
-        Assert.Equal("Domain Administrator", response.TechnicalContact.Name);
-        Assert.Equal("CentralNic Ltd", response.TechnicalContact.Organization);
-        Assert.Equal("+44.2033880600", response.TechnicalContact.TelephoneNumber);
-        Assert.Equal("+44.2033880601", response.TechnicalContact.FaxNumber);
-        Assert.Equal("domains@centralnic.com", response.TechnicalContact.Email);
+        Assert.Null(response.TechnicalContact);
 
         // TechnicalContact Address
-        Assert.Equal(4, response.TechnicalContact.Address.Count);
-        Assert.Equal("35-39 Moorgate", response.TechnicalContact.Address[0]);
-        Assert.Equal("London", response.TechnicalContact.Address[1]);
-        Assert.Equal("EC2R 6AR", response.TechnicalContact.Address[2]);
-        Assert.Equal("GB", response.TechnicalContact.Address[3]);
 
 
         // Nameservers
-        Assert.Equal(6, response.NameServers.Count);
-        Assert.Equal("ns0.centralnic-dns.com", response.NameServers[0]);
-        Assert.Equal("ns1.centralnic-dns.com", response.NameServers[1]);
-        Assert.Equal("ns2.centralnic-dns.com", response.NameServers[2]);
-        Assert.Equal("ns3.centralnic-dns.com", response.NameServers[3]);
-        Assert.Equal("ns4.centralnic-dns.com", response.NameServers[4]);
-        Assert.Equal("ns5.centralnic-dns.com", response.NameServers[5]);
+        Assert.Equal(2, response.NameServers.Count);
+        Assert.Equal("ns03.trs-dns.com", response.NameServers[0]);
+        Assert.Equal("ns03.trs-dns.net", response.NameServers[1]);
 
         // Domain Status
-        Assert.Equal(1, response.DomainStatus.Count);
-        Assert.Equal("ok", response.DomainStatus[0]);
+        Assert.Equal(4, response.DomainStatus.Count);
+        Assert.Equal("serverTransferProhibited", response.DomainStatus[0]);
 
-        Assert.Equal("unsigned", response.DnsSecStatus);
-        Assert.Equal(58, response.FieldsParsed);
+        Assert.Equal("signedDelegation", response.DnsSecStatus);
+        Assert.Equal(19, response.FieldsParsed);
     }
 }
