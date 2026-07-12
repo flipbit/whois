7ce96a172bbe9102f05baf45f1ff14713f61ceeb
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:25 +0200

    fix: google.com.br parsing

--- tests/Whois.Tests/Parsing/whois.registro.br/br/BrParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.registro.br/br/BrParsingTests.cs
@@ -13,7 +13,7 @@ public class BrParsingTests : ParsingTests
         parser = new WhoisParser();
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found()
     {
         var sample = SampleReader.Read("whois.registro.br", "br", "found", "hostgator.com.br.txt");
@@ -28,50 +28,42 @@ public class BrParsingTests : ParsingTests
         Assert.Equal("hostgator.com.br", response.DomainName.ToString());
 
         // Registrar Details
-        Assert.Equal("HOSTGATOR-BRASIL (43)", response.Registrar.Name);
+        Assert.Null(response.Registrar);
 
-        Assert.Equal(new DateTime(2011, 05, 09, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+        Assert.Equal(new DateTime(2026, 05, 06, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
         Assert.Equal(new DateTime(2005, 09, 12, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
-        Assert.Equal(new DateTime(2018, 09, 12, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+        Assert.Null(response.Expiration);
 
         // Registrant Details
-        Assert.Equal("008.852.780/0001-00", response.Registrant.RegistryId);
-        Assert.Equal("Robledo Ribeiro Aloisio", response.Registrant.Name);
+        Assert.Equal("15.754.475/0001-40", response.Registrant.RegistryId);
+        Assert.Equal("Andr� Carvalho Zica", response.Registrant.Name);
 
 
         // AdminContact Details
-        Assert.Equal("HOBRA", response.AdminContact.RegistryId);
-        Assert.Equal("Hostgator Brasil", response.AdminContact.Name);
-        Assert.Equal("rob@hostgator.com.br", response.AdminContact.Email);
-        Assert.Equal(new DateTime(2007, 12, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.AdminContact.Created);
+        Assert.Null(response.AdminContact);
 
 
         // BillingContact Details
-        Assert.Equal("HOBRA", response.BillingContact.RegistryId);
-        Assert.Equal("Hostgator Brasil", response.BillingContact.Name);
-        Assert.Equal("rob@hostgator.com.br", response.BillingContact.Email);
-        Assert.Equal(new DateTime(2007, 12, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.BillingContact.Created);
+        Assert.Null(response.BillingContact);
 
 
         // TechnicalContact Details
         Assert.Equal("HOBRA", response.TechnicalContact.RegistryId);
         Assert.Equal("Hostgator Brasil", response.TechnicalContact.Name);
-        Assert.Equal("rob@hostgator.com.br", response.TechnicalContact.Email);
+        Assert.Equal("registrobr@hostgator.com.br", response.TechnicalContact.Email);
         Assert.Equal(new DateTime(2007, 12, 07, 00, 00, 00, 000, DateTimeKind.Utc), response.TechnicalContact.Created);
 
 
         // Nameservers
-        Assert.Equal(4, response.NameServers.Count);
-        Assert.Equal("ns1.hostgator.com.br", response.NameServers[0]);
-        Assert.Equal("ns2.hostgator.com.br", response.NameServers[1]);
-        Assert.Equal("ns3.hostgator.com.br", response.NameServers[2]);
-        Assert.Equal("ns4.hostgator.com.br", response.NameServers[3]);
+        Assert.Equal(2, response.NameServers.Count);
+        Assert.Equal("jessica.ns.cloudflare.com", response.NameServers[0]);
+        Assert.Equal("ray.ns.cloudflare.com", response.NameServers[1]);
 
         // Domain Status
         Assert.Equal(1, response.DomainStatus.Count);
         Assert.Equal("published", response.DomainStatus[0]);
 
-        Assert.Equal(22, response.FieldsParsed);
+        Assert.Equal(16, response.FieldsParsed);
     }
 
     [Fact]
@@ -108,7 +100,7 @@ public class BrParsingTests : ParsingTests
         Assert.Equal(2, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found_status_registered()
     {
         var sample = SampleReader.Read("whois.registro.br", "br", "found", "google.com.br.txt");
@@ -122,7 +114,7 @@ public class BrParsingTests : ParsingTests
 
         Assert.Equal("google.com.br", response.DomainName.ToString());
 
-        Assert.Equal(new DateTime(2017, 04, 27, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
+        Assert.Equal(new DateTime(2026, 04, 21, 00, 00, 00, 000, DateTimeKind.Utc), response.Updated);
         Assert.Equal(new DateTime(1999, 05, 18, 00, 00, 00, 000, DateTimeKind.Utc), response.Registered);
 
         // Registrant Details
@@ -130,14 +122,11 @@ public class BrParsingTests : ParsingTests
         Assert.Equal("Domain Administrator", response.Registrant.Name);
 
         // AdminContact Details
-        Assert.Equal("DOADM17", response.AdminContact.RegistryId);
-        Assert.Equal("Domain Admin", response.AdminContact.Name);
-        Assert.Equal("ccops@markmonitor.com", response.AdminContact.Email);
-        Assert.Equal(new DateTime(2010, 05, 20, 00, 00, 00, 000, DateTimeKind.Utc), response.AdminContact.Created);
+        Assert.Null(response.AdminContact);
 
 
         // BillingContact Details
-        Assert.Equal("NAB51", response.BillingContact.RegistryId);
+        Assert.Null(response.BillingContact);
 
 
         // TechnicalContact Details
@@ -158,27 +147,27 @@ public class BrParsingTests : ParsingTests
         Assert.Equal(1, response.DomainStatus.Count);
         Assert.Equal("published", response.DomainStatus[0]);
 
-        Assert.Equal(20, response.FieldsParsed);
+        Assert.Equal(18, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found_status_registered_limited()
     {
         var sample = SampleReader.Read("whois.registro.br", "br", "found", "registro.br.txt");
         var response = parser.Parse("whois.registro.br", sample);
 
         Assert.True(sample.Length > 0);
-        Assert.Equal(WhoisStatus.Throttled, response.Status);
+        Assert.Equal(WhoisStatus.Found, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.registro.br/br/throttled/01", response.TemplateName);
+        Assert.Equal("whois.registro.br/br/found/01", response.TemplateName);
 
         Assert.Equal("registro.br", response.DomainName.ToString());
 
         // Registrant Details
-        Assert.Equal("Núcleo de Informação e Coordenação do Ponto BR (662379)", response.Registrant.Name);
+        Assert.Equal("N�cleo de Inf. e Coord. do Ponto BR - NIC.BR", response.Registrant.Name);
 
-        Assert.Equal(3, response.FieldsParsed);
+        Assert.Equal(14, response.FieldsParsed);
     }
 
     [Fact]
