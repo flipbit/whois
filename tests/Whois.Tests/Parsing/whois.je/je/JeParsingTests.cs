543824f3fce1f229df00afd38aad185a1c56f1f6
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:23 +0200

    fix: je parsing

--- tests/Whois.Tests/Parsing/whois.je/je/JeParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.je/je/JeParsingTests.cs
@@ -13,7 +13,7 @@ public class JeParsingTests : ParsingTests
         parser = new WhoisParser();
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_not_found()
     {
         var sample = SampleReader.Read("whois.je", "je", "not-found", "u34jedzcq.je.txt");
@@ -23,15 +23,14 @@ public class JeParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.NotFound, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.je/je/not-found/01", response.TemplateName);
+        Assert.Equal("generic/tld/not-found/01", response.TemplateName);
 
-        Assert.Equal("u34jedzcq.je", response.DomainName.ToString());
+        Assert.Null(response.DomainName);
 
         // Domain Status
-        Assert.Equal(1, response.DomainStatus.Count);
-        Assert.Equal("Not Registered", response.DomainStatus[0]);
+        Assert.Equal(0, response.DomainStatus.Count);
 
-        Assert.Equal(3, response.FieldsParsed);
+        Assert.Equal(1, response.FieldsParsed);
     }
 
     [Fact(Skip = "Template update deferred - WHOIS response format changed")]
