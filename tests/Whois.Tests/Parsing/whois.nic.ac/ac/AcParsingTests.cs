d236ebdf6fbc70b3ffb470ca56948dad8dcc6a49
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:23 +0200

    fix: google.ac parsing

--- tests/Whois.Tests/Parsing/whois.nic.ac/ac/AcParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.nic.ac/ac/AcParsingTests.cs
@@ -13,7 +13,7 @@ public class AcParsingTests : ParsingTests
         parser = new WhoisParser();
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_not_found()
     {
         var sample = SampleReader.Read("whois.nic.ac", "ac", "not-found", "u34jedzcq.ac.txt");
@@ -23,14 +23,14 @@ public class AcParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.NotFound, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.nic.ac/ac/not-found/01", response.TemplateName);
+        Assert.Equal("generic/tld/not-found/03", response.TemplateName);
 
-        Assert.Equal("u34jedzcq.ac", response.DomainName.ToString());
+        Assert.Null(response.DomainName);
 
-        Assert.Equal(2, response.FieldsParsed);
+        Assert.Equal(1, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found()
     {
         var sample = SampleReader.Read("whois.nic.ac", "ac", "found", "google.ac.txt");
@@ -40,35 +40,35 @@ public class AcParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.Found, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.nic.ac/ac/found/01", response.TemplateName);
+        Assert.Equal("generic/tld/found/01", response.TemplateName);
 
         Assert.Equal("google.ac", response.DomainName.ToString());
 
-        Assert.Equal(new DateTime(2014, 04, 03, 00, 00, 00, 000, DateTimeKind.Utc), response.Expiration);
+        Assert.Equal(new DateTime(2027, 04, 03, 13, 38, 02, 000, DateTimeKind.Utc), response.Expiration);
 
         // Registrant Details
-        Assert.Equal("DNS Admin", response.Registrant.Name);
-        Assert.Equal("Google Inc.", response.Registrant.Organization);
+        Assert.Equal("REDACTED", response.Registrant.Name);
+        Assert.Equal("Google LLC", response.Registrant.Organization);
 
         // Registrant Address
-        Assert.Equal(4, response.Registrant.Address.Count);
-        Assert.Equal("1600 Amphitheatre Parkway", response.Registrant.Address[0]);
-        Assert.Equal("Mountain View", response.Registrant.Address[1]);
+        Assert.Equal(5, response.Registrant.Address.Count);
+        Assert.Equal("REDACTED", response.Registrant.Address[0]);
+        Assert.Equal("REDACTED", response.Registrant.Address[1]);
         Assert.Equal("CA", response.Registrant.Address[2]);
-        Assert.Equal("US", response.Registrant.Address[3]);
+        Assert.Equal("REDACTED", response.Registrant.Address[3]);
 
 
         // Nameservers
         Assert.Equal(4, response.NameServers.Count);
         Assert.Equal("ns1.google.com", response.NameServers[0]);
-        Assert.Equal("ns2.google.com", response.NameServers[1]);
+        Assert.Equal("ns4.google.com", response.NameServers[1]);
         Assert.Equal("ns3.google.com", response.NameServers[2]);
-        Assert.Equal("ns4.google.com", response.NameServers[3]);
+        Assert.Equal("ns2.google.com", response.NameServers[3]);
 
         // Domain Status
-        Assert.Equal(1, response.DomainStatus.Count);
-        Assert.Equal("Live", response.DomainStatus[0]);
+        Assert.Equal(6, response.DomainStatus.Count);
+        Assert.Equal("clientDeleteProhibited", response.DomainStatus[0]);
 
-        Assert.Equal(14, response.FieldsParsed);
+        Assert.Equal(47, response.FieldsParsed);
     }
 }
