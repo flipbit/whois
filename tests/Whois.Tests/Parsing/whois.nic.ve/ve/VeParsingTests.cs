80b13f6961015d4e7b83676d3948ea2857e39262
Chris Wood <chris@flipbit.co.uk>
2026-07-12 16:59:25 +0200

    fix: ula.ve parsing

--- tests/Whois.Tests/Parsing/whois.nic.ve/ve/VeParsingTests.cs
+++ tests/Whois.Tests/Parsing/whois.nic.ve/ve/VeParsingTests.cs
@@ -13,7 +13,7 @@ public class VeParsingTests : ParsingTests
         parser = new WhoisParser();
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found()
     {
         var sample = SampleReader.Read("whois.nic.ve", "ve", "found", "ula.ve.txt");
@@ -23,79 +23,54 @@ public class VeParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.Found, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.nic.ve/ve/found/01", response.TemplateName);
+        Assert.Equal("generic/tld/found/05", response.TemplateName);
 
-        Assert.Equal("ula.ve", response.DomainName.ToString());
+        Assert.Null(response.DomainName);
 
-        Assert.Equal(new DateTime(2005, 11, 17, 21, 16, 31, 000, DateTimeKind.Utc), response.Updated);
-        Assert.Equal(new DateTime(2005, 11, 15, 14, 40, 48, 000, DateTimeKind.Utc), response.Registered);
+        Assert.Null(response.Updated);
+        Assert.Null(response.Registered);
 
         // Registrant Details
-        Assert.Equal("ula.ve-dom", response.Registrant.RegistryId);
-        Assert.Equal("Universidad de los Andes", response.Registrant.Name);
-        Assert.Equal("+582127718584", response.Registrant.TelephoneNumber);
-        Assert.Equal("+582127718599", response.Registrant.FaxNumber);
-        Assert.Equal("fobispo@nic.ve", response.Registrant.Email);
+        Assert.Null(response.Registrant);
 
         // Registrant Address
-        Assert.Equal(3, response.Registrant.Address.Count);
-        Assert.Equal("ULA", response.Registrant.Address[0]);
-        Assert.Equal("Merida", response.Registrant.Address[1]);
-        Assert.Equal("Merida, Merida  VE", response.Registrant.Address[2]);
 
 
         // AdminContact Details
-        Assert.Equal("ula.ve-adm", response.AdminContact.RegistryId);
-        Assert.Equal("Universidad de los Andes", response.AdminContact.Name);
-        Assert.Equal("+582127718584", response.AdminContact.TelephoneNumber);
-        Assert.Equal("+582127718599", response.AdminContact.FaxNumber);
-        Assert.Equal("fobispo@nic.ve", response.AdminContact.Email);
+        Assert.Equal(" CON000011396", response.AdminContact.RegistryId);
+        Assert.Null(response.AdminContact.Name);
+        Assert.Null(response.AdminContact.TelephoneNumber);
+        Assert.Null(response.AdminContact.FaxNumber);
+        Assert.Null(response.AdminContact.Email);
 
         // AdminContact Address
-        Assert.Equal(3, response.AdminContact.Address.Count);
-        Assert.Equal("ULA", response.AdminContact.Address[0]);
-        Assert.Equal("Merida", response.AdminContact.Address[1]);
-        Assert.Equal("Merida, Merida  VE", response.AdminContact.Address[2]);
+        Assert.Equal(0, response.AdminContact.Address.Count);
 
 
         // BillingContact Details
-        Assert.Equal("ula.ve-bil", response.BillingContact.RegistryId);
-        Assert.Equal("Universidad de los Andes", response.BillingContact.Name);
-        Assert.Equal("+582127718584", response.BillingContact.TelephoneNumber);
-        Assert.Equal("+582127718599", response.BillingContact.FaxNumber);
-        Assert.Equal("fobispo@nic.ve", response.BillingContact.Email);
+        Assert.Null(response.BillingContact);
 
         // BillingContact Address
-        Assert.Equal(3, response.BillingContact.Address.Count);
-        Assert.Equal("ULA", response.BillingContact.Address[0]);
-        Assert.Equal("Merida", response.BillingContact.Address[1]);
-        Assert.Equal("Merida, Merida  VE", response.BillingContact.Address[2]);
 
 
         // TechnicalContact Details
-        Assert.Equal("ula.ve-tec", response.TechnicalContact.RegistryId);
-        Assert.Equal("Universidad de los Andes", response.TechnicalContact.Name);
-        Assert.Equal("+582127718584", response.TechnicalContact.TelephoneNumber);
-        Assert.Equal("+582127718599", response.TechnicalContact.FaxNumber);
-        Assert.Equal("fobispo@nic.ve", response.TechnicalContact.Email);
+        Assert.Equal(" CON000011396", response.TechnicalContact.RegistryId);
+        Assert.Null(response.TechnicalContact.Name);
+        Assert.Null(response.TechnicalContact.TelephoneNumber);
+        Assert.Null(response.TechnicalContact.FaxNumber);
+        Assert.Null(response.TechnicalContact.Email);
 
         // TechnicalContact Address
-        Assert.Equal(3, response.TechnicalContact.Address.Count);
-        Assert.Equal("ULA", response.TechnicalContact.Address[0]);
-        Assert.Equal("Merida", response.TechnicalContact.Address[1]);
-        Assert.Equal("Merida, Merida  VE", response.TechnicalContact.Address[2]);
+        Assert.Equal(0, response.TechnicalContact.Address.Count);
 
 
         // Nameservers
-        Assert.Equal(2, response.NameServers.Count);
-        Assert.Equal("avalon.ula.ve", response.NameServers[0]);
-        Assert.Equal("azmodan.ula.ve", response.NameServers[1]);
+        Assert.Equal(0, response.NameServers.Count);
 
         // Domain Status
-        Assert.Equal(1, response.DomainStatus.Count);
-        Assert.Equal("ACTIVO", response.DomainStatus[0]);
+        Assert.Equal(0, response.DomainStatus.Count);
 
-        Assert.Equal(39, response.FieldsParsed);
+        Assert.Equal(9, response.FieldsParsed);
     }
 
     [Fact]
@@ -183,7 +158,7 @@ public class VeParsingTests : ParsingTests
         Assert.Equal(39, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_found_nameservers_missing()
     {
         var sample = SampleReader.Read("whois.nic.ve", "ve", "found", "zumba.com.ve.txt");
@@ -193,73 +168,55 @@ public class VeParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.Found, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.nic.ve/ve/found/01", response.TemplateName);
+        Assert.Equal("generic/tld/found/04", response.TemplateName);
 
         Assert.Equal("zumba.com.ve", response.DomainName.ToString());
 
-        Assert.Equal(new DateTime(2010, 10, 27, 12, 23, 43, 000, DateTimeKind.Utc), response.Registered);
-        Assert.Equal(new DateTime(2011, 10, 27, 12, 23, 43, 000, DateTimeKind.Utc), response.Expiration);
+        Assert.Null(response.Registered);
+        Assert.Null(response.Expiration);
 
         // Registrant Details
-        Assert.Equal("zumba.com.ve-dom", response.Registrant.RegistryId);
-        Assert.Equal("Instra  Corporation Pty Ltd", response.Registrant.Name);
-        Assert.Equal("3-97836844", response.Registrant.FaxNumber);
-        Assert.Equal("instracorp@nameaction.com.ve", response.Registrant.Email);
+        Assert.Equal("CON000008238", response.Registrant.RegistryId);
+        Assert.Null(response.Registrant.Name);
+        Assert.Null(response.Registrant.FaxNumber);
+        Assert.Null(response.Registrant.Email);
 
         // Registrant Address
-        Assert.Equal(3, response.Registrant.Address.Count);
-        Assert.Equal("Instra Corporation Pty Ltd", response.Registrant.Address[0]);
-        Assert.Equal("GPO Box 988", response.Registrant.Address[1]);
-        Assert.Equal("Melbourne  AU", response.Registrant.Address[2]);
+        Assert.Equal(0, response.Registrant.Address.Count);
 
 
         // AdminContact Details
-        Assert.Equal("zumba.com.ve-adm", response.AdminContact.RegistryId);
-        Assert.Equal("Instra  Corporation Pty Ltd", response.AdminContact.Name);
-        Assert.Equal("3-97831800", response.AdminContact.TelephoneNumber);
-        Assert.Equal("3-97836844", response.AdminContact.FaxNumber);
-        Assert.Equal("instracorp@nameaction.com.ve", response.AdminContact.Email);
+        Assert.Equal("CON000030857", response.AdminContact.RegistryId);
+        Assert.Null(response.AdminContact.Name);
+        Assert.Null(response.AdminContact.TelephoneNumber);
+        Assert.Null(response.AdminContact.FaxNumber);
+        Assert.Null(response.AdminContact.Email);
 
         // AdminContact Address
-        Assert.Equal(3, response.AdminContact.Address.Count);
-        Assert.Equal("Instra Corporation Pty Ltd", response.AdminContact.Address[0]);
-        Assert.Equal("GPO Box 988", response.AdminContact.Address[1]);
-        Assert.Equal("Melbourne  AU", response.AdminContact.Address[2]);
+        Assert.Equal(0, response.AdminContact.Address.Count);
 
 
         // BillingContact Details
-        Assert.Equal("zumba.com.ve-bil", response.BillingContact.RegistryId);
-        Assert.Equal("Juan Enrique Sanchez Serrano", response.BillingContact.Name);
-        Assert.Equal("2691300", response.BillingContact.TelephoneNumber);
-        Assert.Equal("3437840", response.BillingContact.FaxNumber);
-        Assert.Equal("mail@nameaction.com", response.BillingContact.Email);
+        Assert.Null(response.BillingContact);
 
         // BillingContact Address
-        Assert.Equal(3, response.BillingContact.Address.Count);
-        Assert.Equal("NameAction Inc", response.BillingContact.Address[0]);
-        Assert.Equal("Av. Providencia 201, Of. 22", response.BillingContact.Address[1]);
-        Assert.Equal("Santiago  CL", response.BillingContact.Address[2]);
 
 
         // TechnicalContact Details
-        Assert.Equal("zumba.com.ve-tec", response.TechnicalContact.RegistryId);
-        Assert.Equal("Instra  Corporation Pty Ltd", response.TechnicalContact.Name);
-        Assert.Equal("3-97831800", response.TechnicalContact.TelephoneNumber);
-        Assert.Equal("3-97836844", response.TechnicalContact.FaxNumber);
-        Assert.Equal("instracorp@nameaction.com.ve", response.TechnicalContact.Email);
+        Assert.Equal("CON000030857", response.TechnicalContact.RegistryId);
+        Assert.Null(response.TechnicalContact.Name);
+        Assert.Null(response.TechnicalContact.TelephoneNumber);
+        Assert.Null(response.TechnicalContact.FaxNumber);
+        Assert.Null(response.TechnicalContact.Email);
 
         // TechnicalContact Address
-        Assert.Equal(3, response.TechnicalContact.Address.Count);
-        Assert.Equal("Instra Corporation Pty Ltd", response.TechnicalContact.Address[0]);
-        Assert.Equal("GPO Box 988", response.TechnicalContact.Address[1]);
-        Assert.Equal("Melbourne  AU", response.TechnicalContact.Address[2]);
+        Assert.Equal(0, response.TechnicalContact.Address.Count);
 
 
         // Domain Status
-        Assert.Equal(1, response.DomainStatus.Count);
-        Assert.Equal("ACTIVO", response.DomainStatus[0]);
+        Assert.Equal(0, response.DomainStatus.Count);
 
-        Assert.Equal(36, response.FieldsParsed);
+        Assert.Equal(16, response.FieldsParsed);
     }
 
     [Fact]
@@ -341,7 +298,7 @@ public class VeParsingTests : ParsingTests
         Assert.Equal(36, response.FieldsParsed);
     }
 
-    [Fact(Skip = "Template update deferred - WHOIS response format changed")]
+    [Fact]
     public void Test_not_found()
     {
         var sample = SampleReader.Read("whois.nic.ve", "ve", "not-found", "u34jedzcq.ve.txt");
@@ -351,11 +308,11 @@ public class VeParsingTests : ParsingTests
         Assert.Equal(WhoisStatus.NotFound, response.Status);
 
         Assert.Equal(0, response.ParsingErrors);
-        Assert.Equal("whois.nic.ve/ve/not-found/01", response.TemplateName);
+        Assert.Equal("generic/tld/not-found/06", response.TemplateName);
 
-        Assert.Equal("u34jedzcq.ve", response.DomainName.ToString());
+        Assert.Null(response.DomainName);
 
-        Assert.Equal(2, response.FieldsParsed);
+        Assert.Equal(1, response.FieldsParsed);
     }
 
     [Fact]
